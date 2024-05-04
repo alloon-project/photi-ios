cfe11cc9ba1f10bb284873fb5d3eb4436b46407d
jungseokyoung-cloud <81402827+jungseokyoung-cloud@users.noreply.github.com>
2024-05-04 21:41:35 +0900

    [FEAT]: TextButton 구현 완료 (#22)
    * [REFACT]: NSAttributedString+Extension 수정
    
    * [FEAT]: Text Button 구현
    
    * [FEAT]: Icon Text Button 구현
    
    * [STYLE]: 주석 수정

---
+++ Projects/DesignSystem/Sources/Button/TextButton/TextButtonType.swift
@@ -0,0 +1,14 @@
+//
+//  TextButtonType.swift
+//  DesignSystem
+//
+//  Created by jung on 5/2/24.
+//  Copyright © 2024 com.alloon. All rights reserved.
+//
+
+/// Text Button의 유형을 나타냅니다.
+public enum TextButtonType {
+  case primary
+  case secondary
+  case gray
+}
