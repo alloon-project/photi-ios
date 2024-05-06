7d215f9ca51aeb01a8553def316f7eabc676b15e
Henry <wcbe9745@naver.com>
2024-05-06 13:25:02 +0900

    Testing PrimaryNavigation

---
+++ Projects/DesignSystem/Sources/Navigations/Top/NavigationType.swift
@@ -0,0 +1,18 @@
+//
+//  PrimaryNavigationType.swift
+//  DesignSystem
+//
+//  Created by 임우섭 on 5/5/24.
+//  Copyright © 2024 com.alloon. All rights reserved.
+//
+
+public enum PrimaryNavigationTextType {
+  case none
+  case left
+  case center
+}
+
+public enum PrimaryNavigationIconType {
+  case one
+  case two
+}
