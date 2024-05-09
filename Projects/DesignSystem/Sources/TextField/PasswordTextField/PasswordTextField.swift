12856a3a9193b16b7bc6779c553935a461ee7981
jungseokyoung-cloud <81402827+jungseokyoung-cloud@users.noreply.github.com>
2024-05-09 13:30:14 +0900

    [FEAT]: PasswordTextField 구현 (#32)

---
+++ Projects/DesignSystem/Sources/TextField/PasswordTextField/PasswordTextField.swift
@@ -0,0 +1,58 @@
+//
+//  PasswordTextField.swift
+//  DesignSystem
+//
+//  Created by jung on 5/8/24.
+//  Copyright © 2024 com.alloon. All rights reserved.
+//
+
+import UIKit
+import Core
+
+/// content를 show 혹은 off 할 수 있는 TextField입니다.
+public final class PasswordTextField: LineTextField {
+  private let secureButton: IconButton = {
+    let iconButton = IconButton(size: .medium)
+    iconButton.selectedTintColor = .gray500
+    iconButton.unSelectedTintColor = .gray500
+    
+    iconButton.unSelectedIcon = UIImage(systemName: "eye.slash.fill")!
+    iconButton.selectedIcon = UIImage(systemName: "eye.fill")!
+    iconButton.invalidateIntrinsicContentSize()
+    
+    return iconButton
+  }()
+  
+  var isSecureTextEntry: Bool {
+    get {
+      return textField.isSecureTextEntry
+    }
+    set {
+      print(newValue)
+      textField.isSecureTextEntry = newValue
+      secureButton.isSelected = !newValue
+    }
+  }
+  
+  // MARK: - Setup UI
+  override func setupUI() {
+    super.setupUI()
+    
+    isSecureTextEntry = true
+    secureButton.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+    
+    textField.setRightView(
+      secureButton,
+      size: CGSize(width: 32, height: 32),
+      leftPdding: 8,
+      rightPadding: 9
+    )
+  }
+}
+
+// MARK: - private Extension
+private extension PasswordTextField {
+  @objc func didTapButton() {
+    textField.isSecureTextEntry.toggle()
+  }
+}
