89fb088d2e791d49ec64d32c7e29a1bda1a3181f
jungseokyoung-cloud <81402827+jungseokyoung-cloud@users.noreply.github.com>
2024-05-11 20:47:59 +0900

    [FEAT]: ToastView 구현 (#44)
    * [FEAT]: TipView 구현
    
    * [FEAT]: ToastView 구현
    
    * [FEAT]: ToastView Present 로직 구현
    
    * [FEAT]: ToastView 터치시 Remove되는 기능 추가

---
+++ Projects/DesignSystem/Sources/Popup/ToastView/ToastView.swift
@@ -0,0 +1,200 @@
+//
+//  ToastView.swift
+//  DesignSystem
+//
+//  Created by jung on 5/10/24.
+//  Copyright © 2024 com.alloon. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+import Core
+
+public final class ToastView: UIView {
+  /// Tip의 위치를 나타냅니다.
+  public enum TipPosition {
+    case none
+    case centerTop, centerBottom
+    case leftTop, leftBottom
+    case rightTop, rightBottom
+  }
+  
+  /// toastView를 띄울 Constraint를 적용합니다.
+  private var toastViewConstraints: ((ConstraintMaker) -> Void)?
+  private let tipPosition: TipPosition
+  private let text: String
+  private let icon: UIImage
+  
+  // MARK: - UI Compnents
+  private let label = UILabel()
+  private let iconView = UIImageView()
+  private lazy var tipView: TipView? = nil
+  private var isRemoved = false
+  
+  // MARK: - Initializers
+  public init(
+    tipPosition: TipPosition,
+    text: String,
+    icon: UIImage = UIImage(systemName: "lightbulb.fill")!
+  ) {
+    self.tipPosition = tipPosition
+    self.text = text
+    self.icon = icon
+    super.init(frame: .zero)
+    
+    setupUI()
+  }
+  
+  @available(*, unavailable)
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  // MARK: - UIResponder Override
+  public override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+    self.remove()
+  }
+}
+
+// MARK: - UI Methods
+private extension ToastView {
+  func setupUI() {
+    self.backgroundColor = .gray900
+    layer.cornerRadius = 16
+    setTipView(for: tipPosition)
+    setIconView(icon)
+    setLabel(text)
+    
+    setViewHierarchy()
+    setConstraints(for: tipPosition)
+  }
+  
+  func setViewHierarchy() {
+    addSubviews(label, iconView)
+    
+    if let tipView = tipView {
+      addSubview(tipView)
+    }
+  }
+  
+  func setConstraints(for position: TipPosition) {
+    iconView.snp.makeConstraints {
+      $0.leading.equalToSuperview().offset(32)
+      $0.top.bottom.equalToSuperview().inset(20)
+      $0.width.height.equalTo(16)
+    }
+    
+    label.snp.makeConstraints {
+      $0.trailing.equalToSuperview().offset(-32)
+      $0.centerY.equalToSuperview()
+      $0.leading.equalTo(iconView.snp.trailing).offset(8)
+    }
+    
+    guard let tipView = tipView else { return }
+    
+    switch position {
+      case .centerTop:
+        tipView.snp.makeConstraints {
+          $0.centerX.equalToSuperview()
+          $0.bottom.equalTo(self.snp.top).offset(5)
+        }
+        
+      case .leftTop:
+        tipView.snp.makeConstraints {
+          $0.leading.equalToSuperview().offset(32)
+          $0.bottom.equalTo(self.snp.top).offset(5)
+        }
+        
+      case .rightTop:
+        tipView.snp.makeConstraints {
+          $0.trailing.equalToSuperview().offset(-32)
+          $0.bottom.equalTo(self.snp.top).offset(5)
+        }
+        
+      case .centerBottom:
+        tipView.snp.makeConstraints {
+          $0.centerX.equalToSuperview()
+          $0.bottom.equalToSuperview().offset(9)
+        }
+        
+      case .leftBottom:
+        tipView.snp.makeConstraints {
+          $0.leading.equalToSuperview().offset(32)
+          $0.bottom.equalToSuperview().offset(9)
+        }
+        
+      case .rightBottom:
+        tipView.snp.makeConstraints {
+          $0.trailing.equalToSuperview().offset(-32)
+          $0.bottom.equalToSuperview().offset(9)
+        }
+        
+      default: break
+    }
+  }
+}
+
+// MARK: - Public Methods
+public extension ToastView {
+  func setConstraints(_ closure: @escaping (_ make: ConstraintMaker) -> Void) {
+    self.toastViewConstraints = closure
+  }
+  
+  func present(
+    to viewController: UIViewController,
+    animted: Bool,
+    duration: CGFloat = 3.0,
+    completion: (() -> Void)? = nil
+  ) {
+    guard let constraint = toastViewConstraints else { return }
+    
+    self.isRemoved = false
+    viewController.view.addSubview(self)
+    self.snp.makeConstraints(constraint)
+    
+    DispatchQueue.main.asyncAfter(deadline: .now() + duration) { [weak self] in
+      guard let self else { return }
+    
+      self.remove(completion)
+    }
+  }
+}
+
+// MARK: - Private Methods
+private extension ToastView {
+  func setTipView(for posision: TipPosition) {
+    switch posision {
+      case .centerTop, .leftTop, .rightTop:
+        tipView = TipView(tipDirection: .top)
+      case .leftBottom, .centerBottom, .rightBottom:
+        tipView = TipView(tipDirection: .bottom)
+      default: break
+    }
+  }
+  
+  func setLabel(_ text: String) {
+    label.attributedText = text.attributedString(
+      font: .body2Bold,
+      color: .alloonWhite
+    )
+  }
+  
+  func setIconView(_ icon: UIImage) {
+    iconView.contentMode = .scaleAspectFit
+    iconView.image = icon
+    iconView.tintColor = .gray200
+  }
+  
+  func remove(_ completion: (() -> Void)? = nil) {
+    guard !isRemoved else { return }
+    
+    self.isRemoved = true
+    UIView.animate(withDuration: 0.4) {
+      self.alpha = 0.0
+    } completion: { _ in
+      self.removeFromSuperview()
+      completion?()
+    }
+  }
+}
