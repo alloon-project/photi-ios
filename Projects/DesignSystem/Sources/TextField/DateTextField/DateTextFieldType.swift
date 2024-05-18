c20824a3c82e78637f8a663476666af28c512e5e
Jung SeokYoung <thrdud0423@gmail.com>
2024-05-18 15:15:53 +0900

    [FEAT]: DateTextField 구현

---
+++ Projects/DesignSystem/Sources/TextField/DateTextField/DateTextFieldType.swift
@@ -0,0 +1,13 @@
+//
+//  DateTextFieldType.swift
+//  DesignSystem
+//
+//  Created by jung on 5/16/24.
+//  Copyright © 2024 com.alloon. All rights reserved.
+//
+
+/// Date TextField의 type입니다. 
+public enum DateTextFieldType {
+  case `default`
+  case helper
+}
