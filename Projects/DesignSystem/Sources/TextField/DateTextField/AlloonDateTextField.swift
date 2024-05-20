59f3b5c2d452c6ab419888648dd04646515ed4a6
Jung SeokYoung <thrdud0423@gmail.com>
2024-05-20 23:36:08 +0900

    [BUG]: 유저 커서이동 관련 수정
    - Interaction 안되게 수정

--- Projects/DesignSystem/Sources/TextField/DateTextField/AlloonDateTextField.swift
+++ Projects/DesignSystem/Sources/TextField/DateTextField/AlloonDateTextField.swift
@@ -10,6 +10,10 @@ import UIKit
 
 final class AlloonDateTextField: AlloonTextField {
   var currentText = ""
+
+  override func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+    !isFirstResponder
+  }
   
   // 커서 조작시, 텍스트 끝으로 고정
   override func closestPosition(to point: CGPoint) -> UITextPosition? {
@@ -18,21 +22,5 @@ final class AlloonDateTextField: AlloonTextField {
     let end = self.position(from: beginning, offset: textcount)
     
     return end
-  }
-  
-  // 붙여넣기, select 안되도록 설정.
-  override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
-    if
-      action == #selector(UIResponderStandardEditActions.paste(_:)) ||
-        action == #selector(UIResponderStandardEditActions.select(_:)) {
-      return false
-    }
-    
-    return super.canPerformAction(action, withSender: sender)
-  }
-  
-  // Selection안되도록 설정.
-  override func selectionRects(for range: UITextRange) -> [UITextSelectionRect] {
-    return []
   }
 }
