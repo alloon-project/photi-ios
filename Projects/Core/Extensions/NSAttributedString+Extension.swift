b0c06d4657060f1c3ee9fc7df9402f922be7e70a
jungseokyoung-cloud <81402827+jungseokyoung-cloud@users.noreply.github.com>
2024-05-21 13:19:07 +0900

    [FEAT]: DateTextField 구현 (#56)
    * [REFACT]: NSAttributedString 범위 지정할 수 있도록 변경
    
    * [FEAT]: String to Date, Date to String 추가
    
    * [REFACT]: AlloonTextField internal로 target제거할 수 있는 메서드 추가
    
    * [REFACT]: DS 폰트 변경된거 적용
    
    * [FEAT]: AlloonDateTextField 구현
    
    * [FEAT]: DateTextField 구현
    
    * [BUG]: 유저 커서이동 관련 수정
    
    - Interaction 안되게 수정
    
    * [BUG]: 인터렉션 사이의 버그 수정

--- Projects/Core/Extensions/NSAttributedString+Extension.swift
+++ Projects/Core/Extensions/NSAttributedString+Extension.swift
@@ -9,23 +9,41 @@
 import UIKit
 
 public extension NSAttributedString {
-  func setValue(_ value: Any, for key: NSAttributedString.Key) -> NSAttributedString {
+  func setValue(
+    _ value: Any,
+    for key: NSAttributedString.Key,
+    range: NSRange
+  ) -> NSAttributedString {
     let attributedString = NSMutableAttributedString(attributedString: self)
     
     attributedString.addAttribute(
       key,
       value: value,
-      range: NSRange(location: 0, length: attributedString.length)
+      range: range
     )
     
     return attributedString
   }
   
-  func setUnderLine() -> NSAttributedString {
-    return setValue(NSUnderlineStyle.single.rawValue, for: .underlineStyle)
+  func setUnderLine(for subString: String? = nil) -> NSAttributedString {
+    var range = NSRange()
+    if let subString = subString {
+      range = (self.string as NSString).range(of: subString)
+    } else {
+      range = NSRange(location: 0, length: self.string.count)
+    }
+    
+    return setValue(NSUnderlineStyle.single.rawValue, for: .underlineStyle, range: range)
   }
   
-  func setColor(_ color: UIColor) -> NSAttributedString {
-    return setValue(color, for: .foregroundColor)
+  func setColor(_ color: UIColor, for subString: String? = nil) -> NSAttributedString {
+    var range = NSRange()
+    if let subString = subString {
+      range = (self.string as NSString).range(of: subString)
+    } else {
+      range = NSRange(location: 0, length: self.string.count)
+    }
+    
+    return setValue(color, for: .foregroundColor, range: range)
   }
 }
