d8c3f7c0cf56fdc23a7399a6f32bc3f821e37fe8
Jung SeokYoung <thrdud0423@gmail.com>
2024-05-21 23:44:17 +0900

    [FEAT]: Login UI 완료

--- Projects/Presentation/LogIn/Implementations/LogInViewController.swift
+++ Projects/Presentation/LogIn/Implementations/LogInViewController.swift
@@ -7,11 +7,81 @@
 //
 
 import UIKit
+import RxSwift
+import SnapKit
+import DesignSystem
+import Core
 
 final class LogInViewController: UIViewController {
+  private let disposeBag = DisposeBag()
+  
+  // MARK: - UI Components
+  private let navigationBar = PrimaryNavigationView(textType: .center, iconType: .one, titleText: "로그인")
+  private let idTextField = LineTextField(placeholder: "아이디를 입력해주세요.", type: .helper)
+  private let passwordTextField = PasswordTextField(placeholder: "비밀번호를 입력해주세요", type: .helper)
+  private let loginButton = FilledRoundButton(type: .primary, size: .xLarge, text: "로그인")
+  private let findView = FindView()
+  private let signUpButton = LineRoundButton(text: "회원가입", type: .primary, size: .xLarge)
+  
+  // MARK: - Life Cycles
   override func viewDidLoad() {
     super.viewDidLoad()
-    self.view.backgroundColor = .red
-    print("viewDidLoad")
+    
+    setupUI()
+  }
+  
+  // MARK: - UIResponder
+  override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    super.touchesEnded(touches, with: event)
+    
+    view.endEditing(true)
+  }
+}
+
+// MARK: - UI Methods
+private extension LogInViewController {
+  func setupUI() {
+    self.navigationController?.navigationBar.isHidden = true
+    self.view.backgroundColor = .white
+
+    setViewHierarchy()
+    setConstraints()
+  }
+  
+  func setViewHierarchy() { 
+    view.addSubviews(navigationBar, idTextField, passwordTextField, loginButton, findView, signUpButton)
+  }
+  
+  func setConstraints() {
+    navigationBar.snp.makeConstraints {
+      $0.leading.trailing.equalToSuperview()
+      $0.top.equalTo(view.safeAreaLayoutGuide)
+      $0.height.equalTo(56)
+    }
+    
+    idTextField.snp.makeConstraints {
+      $0.centerX.equalToSuperview()
+      $0.top.equalTo(navigationBar.snp.bottom).offset(40)
+    }
+    
+    passwordTextField.snp.makeConstraints {
+      $0.centerX.equalToSuperview()
+      $0.top.equalTo(idTextField.snp.bottom).offset(16)
+    }
+    
+    loginButton.snp.makeConstraints {
+      $0.centerX.equalToSuperview()
+      $0.top.equalTo(passwordTextField.snp.bottom).offset(40)
+    }
+    
+    findView.snp.makeConstraints {
+      $0.centerX.equalToSuperview()
+      $0.top.equalTo(loginButton.snp.bottom).offset(25)
+    }
+    
+    signUpButton.snp.makeConstraints {
+      $0.centerX.equalToSuperview()
+      $0.bottom.equalToSuperview().offset(-56)
+    }
   }
 }
--- Projects/Presentation/LogIn/Implementations/Views/FindView.swift
+++ Projects/Presentation/LogIn/Implementations/Views/FindView.swift
@@ -6,4 +6,73 @@
 //  Copyright © 2024 com.alloon. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import RxCocoa
+import RxSwift
+import SnapKit
+import Core
+import DesignSystem
+
+final class FindView: UIView {
+  // MARK: - UI Components
+  fileprivate let findIdButton = TextButton(text: "아이디 찾기", size: .xSmall, type: .gray)
+  fileprivate let findPasswordButton = TextButton(text: "비밀번호 찾기", size: .xSmall, type: .gray)
+  
+  private let verticalLine: UIView = {
+    let view = UIView()
+    view.backgroundColor = .gray300
+    
+    return view
+  }()
+  
+  // MARK: - Initializers
+  init() {
+    super.init(frame: .zero)
+    setupUI()
+  }
+  
+  @available(*, unavailable)
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
+
+// MARK: - UI Methods
+private extension FindView {
+  func setupUI() {
+    setViewHeirarchy()
+    setConstraints()
+  }
+  
+  func setViewHeirarchy() {
+    self.addSubviews(findIdButton, verticalLine, findPasswordButton)
+  }
+  
+  func setConstraints() {
+    findIdButton.snp.makeConstraints {
+      $0.leading.top.bottom.equalToSuperview()
+    }
+    
+    verticalLine.snp.makeConstraints {
+      $0.width.equalTo(1)
+      $0.height.equalTo(10)
+      $0.centerY.equalToSuperview()
+      $0.leading.equalTo(findIdButton.snp.trailing).offset(8)
+    }
+    
+    findPasswordButton.snp.makeConstraints {
+      $0.leading.equalTo(verticalLine.snp.trailing).offset(8)
+      $0.trailing.top.bottom.equalToSuperview()
+    }
+  }
+}
+
+extension Reactive where Base: FindView {
+  var didTapFindIdButton: ControlEvent<Void> {
+    base.findIdButton.rx.tap
+  }
+  
+  var didTapFindPasswordButton: ControlEvent<Void> {
+    base.findPasswordButton.rx.tap
+  }
+}
