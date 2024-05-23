d35de2b158aa430b39be99be1ab58386a9679a3d
Jung SeokYoung <thrdud0423@gmail.com>
2024-05-23 20:56:14 +0900

    [FEAT]: EmailEnterViewController UI 구현 완료

--- Projects/Presentation/LogIn/Implementations/SignUp/EnterEmail/EnterEmailViewController.swift
+++ Projects/Presentation/LogIn/Implementations/SignUp/EnterEmail/EnterEmailViewController.swift
@@ -17,6 +17,34 @@ final class EnterEmailViewController: UIViewController {
   private let disposeBag = DisposeBag()
   private let viewModel: EnterEmailViewModel
   
+  // MARK: - UI Components
+  private let navigationBar = PrimaryNavigationView(textType: .none, iconType: .one)
+  private let progressBar = LargeProgressBar(step: .one)
+  private let titleLabel: UILabel = {
+    let label = UILabel()
+    label.attributedText = "환영합니다!\n이메일을 입력해주세요".attributedString(
+      font: .heading4,
+      color: .gray900
+    )
+    label.numberOfLines = 2
+    
+    return label
+  }()
+  
+  // TODO: - emailTextField
+  private let lineTextField = LineTextField(placeholder: "이메일", type: .helper)
+  private let nextButton = FilledRoundButton(type: .primary, size: .xLarge, text: "다음")
+  // TODO: - DS 적용 후 이미지 변경
+  private let emailFormWarningView = CommentView(
+    .warning, text: "이메일 형태가 올바르지 않아요", icon: UIImage(systemName: "xmark")!
+  )
+  private let emailTextCountWarningView = CommentView(
+    .warning, text: "100자 이하의 이메일을 사용해주세요", icon: UIImage(systemName: "xmark")!
+  )
+  private let duplicateEmailWarningView = CommentView(
+    .warning, text: "이미 가입된 이메일이예요", icon: UIImage(systemName: "xmark")!
+  )
+  
   // MARK: - Initialziers
   init(viewModel: EnterEmailViewModel) {
     self.viewModel = viewModel
@@ -26,5 +54,62 @@ final class EnterEmailViewController: UIViewController {
   @available(*, unavailable)
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
+  }
+  
+  // MARK: - Life Cylces
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    setupUI()
+    bind()
+  }
+  
+  // MARK: - UI Responder
+  override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    super.touchesEnded(touches, with: event)
+    
+    view.endEditing(true)
+  }
+}
+
+// MARK: - UI Methods
+private extension EnterEmailViewController {
+  func setupUI() {
+    view.backgroundColor = .white
+    setViewHierarchy()
+    setConstraints()
+  }
+  
+  func setViewHierarchy() {
+    view.addSubviews(navigationBar, progressBar, titleLabel, lineTextField, nextButton)
+  }
+  
+  func setConstraints() {
+    navigationBar.snp.makeConstraints {
+      $0.leading.trailing.equalToSuperview()
+      $0.top.equalTo(view.safeAreaLayoutGuide)
+      $0.height.equalTo(56)
+    }
+    
+    progressBar.snp.makeConstraints {
+      $0.top.equalTo(navigationBar.snp.bottom).offset(8)
+      $0.leading.trailing.equalToSuperview().inset(24)
+    }
+    
+    titleLabel.snp.makeConstraints {
+      $0.top.equalTo(progressBar.snp.bottom).offset(48)
+      $0.leading.equalToSuperview().offset(24)
+    }
+    
+    lineTextField.snp.makeConstraints {
+      $0.centerX.equalToSuperview()
+      $0.top.equalTo(titleLabel.snp.bottom).offset(24)
+      $0.leading.trailing.equalToSuperview().inset(24)
+    }
+    
+    nextButton.snp.makeConstraints {
+      $0.centerX.equalToSuperview()
+      $0.bottom.equalToSuperview().offset(-56)
+    }
   }
 }
