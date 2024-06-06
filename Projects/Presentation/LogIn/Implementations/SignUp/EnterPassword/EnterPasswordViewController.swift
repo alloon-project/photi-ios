be82cc361d8f9ba4b913c642d5f1af99139a0390
Jung SeokYoung <thrdud0423@gmail.com>
2024-06-06 15:00:32 +0900

    [FEAT]: EnterPassword UI로직 완료

--- Projects/Presentation/LogIn/Implementations/SignUp/EnterPassword/EnterPasswordViewController.swift
+++ Projects/Presentation/LogIn/Implementations/SignUp/EnterPassword/EnterPasswordViewController.swift
@@ -14,7 +14,13 @@ import Core
 import DesignSystem
 
 final class EnterPasswordViewController: UIViewController {
+  private let disposeBag = DisposeBag()
   private let viewModel: EnterPasswordViewModel
+  
+  private let bottomSheetTitle = "얼른 서비스 이용을 위한\n필수 약관에 동의해주세요"
+  private let bottomSheetDataSource = ["서비스 이용약관 동의", "개인정보 수집 및 이용 동의"]
+  
+  private let didTapContinueButton = PublishRelay<Void>()
   
   // MARK: - UI Components
   private let navigationBar = PrimaryNavigationView(textType: .none, iconType: .one)
@@ -48,7 +54,7 @@ final class EnterPasswordViewController: UIViewController {
   
   // TODO: - DS 적용후 이미지 수정
   private let containAlphabetCommentView = CommentView(
-    .condition, text: "숫자 포함", icon: UIImage(systemName: "checkmark")!
+    .condition, text: "영문 포함", icon: UIImage(systemName: "checkmark")!
   )
   private let containNumberCommentView = CommentView(
     .condition, text: "숫자 포함", icon: UIImage(systemName: "checkmark")!
@@ -59,7 +65,7 @@ final class EnterPasswordViewController: UIViewController {
   private let validRangeCommentView = CommentView(
     .condition, text: "8~30자", icon: UIImage(systemName: "checkmark")!
   )
-
+  
   private let correnspondPasswordCommentView = CommentView(
     .condition, text: "비밀번호 일치", icon: UIImage(systemName: "checkmark")!
   )
@@ -81,6 +87,7 @@ final class EnterPasswordViewController: UIViewController {
     super.viewDidLoad()
     
     setupUI()
+    bind()
   }
   
   // MARK: - UIResponder
@@ -93,24 +100,24 @@ final class EnterPasswordViewController: UIViewController {
 
 // MARK: - UI Methods
 private extension EnterPasswordViewController {
-  func setupUI() { 
+  func setupUI() {
     self.view.backgroundColor = .white
     passwordTextField.commentViews = [
       containAlphabetCommentView, containNumberCommentView, containSpecialCommentView, validRangeCommentView
     ]
     
     passwordCheckTextField.commentViews = [correnspondPasswordCommentView]
-                                            
+    
     setViewHierarchy()
     setConstraints()
   }
   
-  func setViewHierarchy() { 
+  func setViewHierarchy() {
     self.view.addSubviews(navigationBar, progressBar, passwordTitleLabel, passwordTextField, nextButton)
     self.view.addSubviews(passwordCheckTitleLabel, passwordCheckTextField)
   }
   
-  func setConstraints() { 
+  func setConstraints() {
     navigationBar.snp.makeConstraints {
       $0.leading.trailing.equalToSuperview()
       $0.top.equalTo(view.safeAreaLayoutGuide)
@@ -146,5 +153,97 @@ private extension EnterPasswordViewController {
       $0.centerX.equalToSuperview()
       $0.bottom.equalToSuperview().offset(-56)
     }
+  }
+}
+
+// MARK: - Bind Methods
+private extension EnterPasswordViewController {
+  func bind() {
+    let input = EnterPasswordViewModel.Input(
+      password: passwordTextField.rx.text,
+      reEnteredPassword: passwordCheckTextField.rx.text,
+      didTapBackButton: navigationBar.rx.didTapLeftButton,
+      didTapContinueButton: ControlEvent(events: didTapContinueButton.asObservable())
+    )
+    
+    nextButton.rx.tap
+      .bind(with: self) { owner, _ in
+        owner.presentBottomSheet()
+      }
+      .disposed(by: disposeBag)
+    
+    let output = viewModel.transform(input: input)
+    bind(output: output)
+  }
+  
+  func bind(output: EnterPasswordViewModel.Output) {
+    output.containAlphabet
+      .drive(containAlphabetCommentView.rx.isActivate)
+      .disposed(by: disposeBag)
+    
+    output.containNumber
+      .drive(containNumberCommentView.rx.isActivate)
+      .disposed(by: disposeBag)
+    
+    output.containSpecial
+      .drive(containSpecialCommentView.rx.isActivate)
+      .disposed(by: disposeBag)
+    
+    output.isValidRange
+      .drive(validRangeCommentView.rx.isActivate)
+      .disposed(by: disposeBag)
+    
+    output.isValidPassword
+      .map { !$0 }
+      .drive(passwordCheckTextField.rx.isHidden)
+      .disposed(by: disposeBag)
+
+    output.isValidPassword
+      .map { !$0 }
+      .drive(passwordCheckTitleLabel.rx.isHidden)
+      .disposed(by: disposeBag)
+    
+    output.isValidPassword
+      .filter { $0 == false }
+      .map { _ in "" }
+      .drive(passwordCheckTextField.rx.text)
+      .disposed(by: disposeBag)
+    
+    output.correspondPassword
+      .drive(correnspondPasswordCommentView.rx.isActivate)
+      .disposed(by: disposeBag)
+    
+    output.isEnabledNextButton
+      .drive(nextButton.rx.isEnabled)
+      .disposed(by: disposeBag)
+  }
+}
+
+// MARK: - Private Methods
+private extension EnterPasswordViewController {
+  func presentBottomSheet() {
+    let alert = ListBottomSheetViewController(
+      title: bottomSheetTitle,
+      button: "동의 후 계속",
+      dataSource: bottomSheetDataSource
+    )
+    alert.delegate = self
+    
+    alert.present(to: self, animated: true) { [weak self] in
+      self?.progressBar.step = .five
+    }
+    
+    alert.didDismiss
+      .map { _ in AlloonProgressStep.four }
+      .bind(to: progressBar.rx.step)
+      .disposed(by: disposeBag)
+  }
+}
+
+extension EnterPasswordViewController: ListBottomSheetDelegate {
+  func didTapIcon(at index: Int) {  }
+  
+  func didTapButton() {
+    didTapContinueButton.accept(())
   }
 }
--- Projects/Presentation/LogIn/Implementations/SignUp/EnterPassword/EnterPasswordViewModel.swift
+++ Projects/Presentation/LogIn/Implementations/SignUp/EnterPassword/EnterPasswordViewModel.swift
@@ -25,15 +25,72 @@ final class EnterPasswordViewModel: EnterPasswordViewModelType {
   weak var coordinator: EnterPasswordCoordinatable?
   
   // MARK: - Input
-  struct Input { }
+  struct Input {
+    var password: ControlProperty<String>
+    var reEnteredPassword: ControlProperty<String>
+    var didTapBackButton: ControlEvent<Void>
+    var didTapContinueButton: ControlEvent<Void>
+  }
   
   // MARK: - Output
-  struct Output { }
+  struct Output {
+    var containAlphabet: Driver<Bool>
+    var containNumber: Driver<Bool>
+    var containSpecial: Driver<Bool>
+    var isValidRange: Driver<Bool>
+    var isValidPassword: Driver<Bool>
+    var correspondPassword: Driver<Bool>
+    var isEnabledNextButton: Driver<Bool>
+  }
   
   // MARK: - Initializers
   init() { }
   
   func transform(input: Input) -> Output {
-    return Output()
+    input.didTapBackButton
+      .bind(with: self) { owner, _ in
+//        owner?.coordinator
+      }
+      .disposed(by: disposeBag)
+    
+    input.didTapContinueButton
+      .bind(with: self) { owner, _ in
+        print("continue")
+      }
+      .disposed(by: disposeBag)
+    
+    let containAlphabet = input.password
+      .map { $0.contain("[a-zA-Z]") }
+    
+    let containNumber = input.password
+      .map { $0.contain("[0-9]") }
+    
+    let containSpecial = input.password
+      .map { $0.contain("[^a-zA-Z0-9]") }
+    
+    let isValidRange = input.password
+      .map { $0.count >= 8 && $0.count <= 30 }
+    
+    let isValidPassword = Observable.combineLatest(
+      containAlphabet, containNumber, containSpecial, isValidRange
+    ) { $0 && $1 && $2 && $3 }
+    
+    let correspondPassword = Observable.combineLatest(
+      input.password, input.reEnteredPassword
+    ) { $0 == $1 }
+    
+    let isEnabledNextButton = Observable.combineLatest(
+      isValidPassword, correspondPassword
+    ) { $0 && $1 }
+    
+    return Output(
+      containAlphabet: containAlphabet.asDriver(onErrorJustReturn: false),
+      containNumber: containNumber.asDriver(onErrorJustReturn: false),
+      containSpecial: containSpecial.asDriver(onErrorJustReturn: false),
+      isValidRange: isValidRange.asDriver(onErrorJustReturn: false),
+      isValidPassword: isValidPassword.asDriver(onErrorJustReturn: false), 
+      correspondPassword: correspondPassword.asDriver(onErrorJustReturn: false),
+      isEnabledNextButton: isEnabledNextButton.asDriver(onErrorJustReturn: false)
+    )
   }
 }
