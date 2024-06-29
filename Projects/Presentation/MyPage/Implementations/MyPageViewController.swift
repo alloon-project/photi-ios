bed3c7197a029dd34b901562c17fcfc1b46b6ffd
Jung SeokYoung <thrdud0423@gmail.com>
2024-06-29 21:47:55 +0900

    [FEAT]: MyPage Coordinator 추가

---
+++ Projects/Presentation/MyPage/Implementations/MyPageViewController.swift
@@ -0,0 +1,11 @@
+//
+//  MyPageViewController.swift
+//  MyPageImpl
+//
+//  Created by jung on 6/29/24.
+//  Copyright © 2024 com.alloon. All rights reserved.
+//
+
+import UIKit
+
+final class MyPageViewController: UIViewController { }
