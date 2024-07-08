1e79c6464f4c2c28a4625825f82a08853785387e
Henry <wcbe9745@naver.com>
2024-07-08 11:42:03 +0900

    Update : ReportType 주입

--- Projects/Presentation/Home/Implementations/Report/ReportCoordinator.swift
+++ Projects/Presentation/Home/Implementations/Report/ReportCoordinator.swift
@@ -19,12 +19,14 @@ protocol ReportListener: AnyObject {
 
 final class ReportCoordinator: Coordinator, ReportCoordinatable {
   weak var listener: ReportListener?
+  private let reportType: ReportType
   
   private let viewController: ReportViewController
   private let viewModel: any ReportViewModelType
   
-  init(viewModel: ReportViewModel) {
+  init(viewModel: ReportViewModel, reportType: ReportType) {
     self.viewModel = viewModel
+    self.reportType = reportType
     self.viewController = ReportViewController(viewModel: viewModel)
     
     super.init()
@@ -33,6 +35,7 @@ final class ReportCoordinator: Coordinator, ReportCoordinatable {
   
   override func start(at navigationController: UINavigationController?) {
     super.start(at: navigationController)
+    viewController.setReportType(type: reportType)
     navigationController?.pushViewController(viewController, animated: false)
   }
   
