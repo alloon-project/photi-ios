de1c671e54840601bc3718227afd580bcbabc92b
Jung SeokYoung <thrdud0423@gmail.com>
2024-08-15 16:15:12 +0900

    [FEAT]: SignUpRepository 작성

---
+++ Projects/Domain/Repository/Interfaces/SignUpRepository.swift
@@ -0,0 +1,16 @@
+//
+//  SignUpRepository.swift
+//  Repository
+//
+//  Created by jung on 8/14/24.
+//  Copyright © 2024 com.photi. All rights reserved.
+//
+
+import RxSwift
+import DataMapper
+
+public protocol SignUpRepository {
+  init(dataMapper: SignUpDataMapper)
+  
+  func requestVerificationCode(email: String) -> Single<Void>
+}
