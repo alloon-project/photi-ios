eb3865ec4b7c9b404996a9551ee8bc575020f2f0
Jung SeokYoung <thrdud0423@gmail.com>
2024-08-19 10:01:10 +0900

    [FEAT]: 이메일 검증 구현 완료

--- Projects/Presentation/LogIn/Implementations/SignUp/EnterEmail/VerifyEmail/VerifyEmailViewModel.swift
+++ Projects/Presentation/LogIn/Implementations/SignUp/EnterEmail/VerifyEmail/VerifyEmailViewModel.swift
@@ -8,8 +8,10 @@
 
 import RxCocoa
 import RxSwift
+import Entity
+import UseCase
 
-protocol VerifyEmailCoordinatable: AnyObject { 
+protocol VerifyEmailCoordinatable: AnyObject {
   func didTapBackButton()
   func didTapNextButton()
 }
@@ -24,26 +26,36 @@ protocol VerifyEmailViewModelType: AnyObject, VerifyEmailViewModelable {
 
 final class VerifyEmailViewModel: VerifyEmailViewModelType {
   let disposeBag = DisposeBag()
-  private var verifyCode: String = ""
+  private let useCase: SignUpUseCase
+  private let email: String
   
   weak var coordinator: VerifyEmailCoordinatable?
   
+  private let requestFailedRelay = PublishRelay<Void>()
+  private let invalidVerificationCodeRelay = PublishRelay<Void>()
+  private let emailNotFoundRelay = PublishRelay<Void>()
+  
   // MARK: - Input
-  struct Input { 
-    var viewDidLoad: PublishRelay<Void>
+  struct Input {
     var didTapBackButton: ControlEvent<Void>
     var didTapResendButton: ControlEvent<Void>
     var didTapNextButton: ControlEvent<Void>
-    var verifyCode: ControlProperty<String>
+    var verificationCode: ControlProperty<String>
   }
   
   // MARK: - Output
-  struct Output { 
+  struct Output {
     var isEnabledNextButton: Signal<Bool>
+    var requestFailed: Signal<Void>
+    var invalidVerificationCode: Signal<Void>
+    var emailNotFound: Signal<Void>
   }
   
   // MARK: - Initializers
-  init() { }
+  init(useCase: SignUpUseCase, email: String) {
+    self.useCase = useCase
+    self.email = email
+  }
   
   func transform(input: Input) -> Output {
     input.didTapBackButton
@@ -51,34 +63,77 @@ final class VerifyEmailViewModel: VerifyEmailViewModelType {
         owner.coordinator?.didTapBackButton()
       }
       .disposed(by: disposeBag)
-    
+
     input.didTapNextButton
+      .withLatestFrom(input.verificationCode)
+      .filter { $0.count == 4 }
+      .bind(with: self) { owner, code in
+        owner.verifyCode(email: owner.email, code: code)
+      }
+      .disposed(by: disposeBag)
+    
+    input.didTapResendButton
       .bind(with: self) { owner, _ in
-        owner.coordinator?.didTapNextButton()
+        owner.requestVerificationCode(email: owner.email)
       }
       .disposed(by: disposeBag)
     
-    input.viewDidLoad
-      .subscribe(with: self) { owner, _ in
-        owner.requestVerifyCode()
-      }
-      .disposed(by: disposeBag)
-    
-    let isEnabledNextButton = input.verifyCode
+    let isEnabledNextButton = input.verificationCode
       .map { $0.count == 4 }
       .asSignal(onErrorJustReturn: false)
     
     return Output(
-      isEnabledNextButton: isEnabledNextButton
+      isEnabledNextButton: isEnabledNextButton,
+      requestFailed: requestFailedRelay.asSignal(),
+      invalidVerificationCode: invalidVerificationCodeRelay.asSignal(),
+      emailNotFound: emailNotFoundRelay.asSignal()
     )
   }
 }
 
 // MARK: - Private Methods
 private extension VerifyEmailViewModel {
-  func requestVerifyCode() {
-    self.verifyCode = ""
-    print(#function)
-    // 새로운 값으로 verifyCode 갱신
+  func requestVerificationCode(email: String) {
+    useCase.requestVerificationCode(email: email)
+      .observe(on: MainScheduler.instance)
+      .subscribe(
+        with: self,
+        onFailure: { owner, error in
+          owner.requestFailed(error: error)
+        }
+      )
+      .disposed(by: disposeBag)
+  }
+  
+  func verifyCode(email: String, code: String) {
+    useCase.verifyCode(email: email, code: code)
+      .observe(on: MainScheduler.instance)
+      .subscribe(
+        with: self,
+        onSuccess: { owner, _ in
+          owner.coordinator?.didTapNextButton()
+        },
+        onFailure: { owner, error in
+          owner.requestFailed(error: error)
+        }
+      )
+      .disposed(by: disposeBag)
+  }
+  
+  func requestFailed(error: Error) {
+    print(error)
+    if
+      let error = error as? APIError,
+      case .signUpFailed(let reason) = error {
+      if case .invalidVerificationCode = reason {
+        invalidVerificationCodeRelay.accept(())
+      } else if case .emailNotFound = reason {
+        emailNotFoundRelay.accept(())
+      } else {
+        requestFailedRelay.accept(())
+      }
+    } else {
+      requestFailedRelay.accept(())
+    }
   }
 }
