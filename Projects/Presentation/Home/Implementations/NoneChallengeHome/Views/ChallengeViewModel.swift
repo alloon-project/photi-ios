3a9008453a82d093852ac16d38835bfe44c50a47
Jung SeokYoung <thrdud0423@gmail.com>
2024-09-19 22:46:00 +0900

    [FEAT]: ChallengeViewModel 추가

---
+++ Projects/Presentation/Home/Implementations/NoneChallengeHome/Views/ChallengeViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ChallengeViewModel.swift
+//  HomeImpl
+//
+//  Created by jung on 9/19/24.
+//  Copyright © 2024 com.photi. All rights reserved.
+//
+
+import UIKit
+
+struct ChallengeViewModel {
+  let image: UIImage?
+}
