257a3aa93993b13e4993b6d683e28402a89dae1c
Jung SeokYoung <thrdud0423@gmail.com>
2024-12-11 01:04:50 +0900

    [FEAT]: Challenge의 상단 UI 구현

---
+++ Projects/Presentation/Challenge/Implementations/PresentationModel/ChallengeTitlePresentationModel.swift
@@ -0,0 +1,16 @@
+//
+//  ChallengeTitlePresentationModel.swift
+//  HomeImpl
+//
+//  Created by jung on 12/10/24.
+//  Copyright © 2024 com.photi. All rights reserved.
+//
+
+import Foundation
+
+struct ChallengeTitlePresentationModel {
+  let challengeId: String
+  let title: String
+  let hashTags: [String]
+  let imageURL: String
+}
