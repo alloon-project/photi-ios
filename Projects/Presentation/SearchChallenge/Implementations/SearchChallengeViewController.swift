0b261f329af503933939fd61a355a8484718e472
Jung SeokYoung <thrdud0423@gmail.com>
2024-12-10 15:01:42 +0900

    [FEAT]: SegmentControl 구현

--- Projects/Presentation/SearchChallenge/Implementations/SearchChallengeViewController.swift
+++ Projects/Presentation/SearchChallenge/Implementations/SearchChallengeViewController.swift
@@ -7,5 +7,30 @@
 //
 
 import UIKit
+import DesignSystem
+import RxCocoa
+import RxSwift
+import SnapKit
 
-final class SearchChallengeViewController: UIViewController { }
+final class SearchChallengeViewController: UIViewController {
+  private let segment = PhotiSegmentControl()
+  private let disposeBag = DisposeBag()
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    segment.items = ["1번", "2번", "3번"]
+    view.addSubview(segment)
+    view.backgroundColor = .gray200
+    segment.snp.makeConstraints {
+      $0.center.equalToSuperview()
+      $0.height.equalTo(50)
+      $0.width.equalTo(380)
+    }
+    
+    segment.rx.selectedSegment
+      .bind { segment in
+        print(segment)
+      }
+      .disposed(by: disposeBag)
+  }
+}
