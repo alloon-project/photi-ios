4f2c5e03462a271bad3120c6eb6e8d1518e02562
Jung SeokYoung <thrdud0423@gmail.com>
2024-12-11 01:06:04 +0900

    [FEAT]: ChallengeViewController 상단 UI 구현 완료

--- Projects/Presentation/Challenge/Implementations/ChallengeViewController.swift
+++ Projects/Presentation/Challenge/Implementations/ChallengeViewController.swift
@@ -7,9 +7,23 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
+import Core
+import DesignSystem
 
 final class ChallengeViewController: UIViewController {
   private let viewModel: ChallengeViewModel
+  private let disposeBag = DisposeBag()
+  
+  // MARK: - UI Components
+  private let navigationBar = PhotiNavigationBar(
+    leftView: .backButton,
+    rigthItems: [.shareButton, .optionButton],
+    displayMode: .white
+  )
+  private let titleView = ChallengeTitleView()
+  private let segmentControl = PhotiSegmentControl(items: ["피드", "소개", "파티원"])
   
   // MARK: - Initializers
   init(viewModel: ChallengeViewModel) {
@@ -20,5 +34,42 @@ final class ChallengeViewController: UIViewController {
   @available(*, unavailable)
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
+  }
+  
+  // MARK: - Life Cycles
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setupUI()
+  }
+}
+
+// MARK: - UI Methods
+private extension ChallengeViewController {
+  func setupUI() {
+    setViewHierarhcy()
+    setConstraints()
+  }
+  
+  func setViewHierarhcy() {
+    view.addSubviews(titleView, navigationBar, segmentControl)
+  }
+  
+  func setConstraints() {
+    navigationBar.snp.makeConstraints {
+      $0.leading.trailing.equalToSuperview()
+      $0.top.equalTo(view.safeAreaLayoutGuide)
+      $0.height.equalTo(56)
+    }
+    
+    titleView.snp.makeConstraints {
+      $0.top.leading.trailing.equalToSuperview()
+      $0.height.equalTo(300)
+    }
+    
+    segmentControl.snp.makeConstraints {
+      $0.bottom.equalTo(titleView)
+      $0.leading.trailing.equalToSuperview()
+      $0.height.equalTo(38)
+    }
   }
 }
