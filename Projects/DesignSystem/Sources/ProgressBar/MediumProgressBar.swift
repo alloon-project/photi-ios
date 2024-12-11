10ec6587bc5396345d1f576b169e9eac92eb0b7e
Jung SeokYoung <thrdud0423@gmail.com>
2024-12-11 17:46:58 +0900

    [REFACT]: MediumProgressBar 리팩토링

--- Projects/DesignSystem/Sources/ProgressBar/MediumProgressBar.swift
+++ Projects/DesignSystem/Sources/ProgressBar/MediumProgressBar.swift
@@ -14,29 +14,19 @@ import SnapKit
 /// ![MediumProgressBar](MediumProgressBar)
 public final class MediumProgressBar: UIProgressView {
   /// 현재 ProgressBar의 Percent값입니다. 변경시 UI도 변경적용됩니다.
-  public var percent: PhotiProgressPercent = .percent0 {
-    didSet {
-      switch percent {
-      case .percent20:
-        self.setProgress(0.2, animated: true)
-      case .percent40:
-        self.setProgress(0.4, animated: true)
-      case .percent60:
-        self.setProgress(0.6, animated: true)
-      case .percent80:
-        self.setProgress(0.8, animated: true)
-      case .percent100:
-        self.setProgress(1.0, animated: true)
-      case .percent0:
-        self.setProgress(0.0, animated: true)
-      }
-    }
+  public var percent: PhotiProgressPercent {
+    didSet { setPercentage(percent) }
   }
   
   // MARK: - Initializers
-  public init() {
+  public init(percent: PhotiProgressPercent) {
+    self.percent = percent
     super.init(frame: .zero)
     setupUI()
+  }
+  
+  public convenience init() {
+    self.init(percent: .percent0)
   }
   
   required init?(coder: NSCoder) {
@@ -59,8 +49,23 @@ private extension MediumProgressBar {
   func setupUI() {
     self.backgroundColor = .gray200
     self.progressTintColor = .green400
-    self.layer.cornerRadius = self.frame.height / 2
-    self.layer.masksToBounds = true
-    self.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMaxXMaxYCorner]
+    setPercentage(percent)
+  }
+  
+  func setPercentage(_ percent: PhotiProgressPercent) {
+    switch percent {
+    case .percent20:
+      self.setProgress(0.2, animated: true)
+    case .percent40:
+      self.setProgress(0.4, animated: true)
+    case .percent60:
+      self.setProgress(0.6, animated: true)
+    case .percent80:
+      self.setProgress(0.8, animated: true)
+    case .percent100:
+      self.setProgress(1.0, animated: true)
+    case .percent0:
+      self.setProgress(0.0, animated: true)
+    }
   }
 }
