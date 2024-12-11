214209ce1ef72911ae120c9e58fe3c1d2a7ceed8
Jung SeokYoung <thrdud0423@gmail.com>
2024-12-11 17:49:17 +0900

    [FEAT]: Challenge에서 Feed 볼 수 있는 Feed Coordinator 추가

---
+++ Projects/Presentation/Challenge/Implementations/Member/Feed/FeedViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FeedViewController.swift
+//  HomeImpl
+//
+//  Created by jung on 12/11/24.
+//  Copyright © 2024 com.photi. All rights reserved.
+//
+
+import UIKit
+import RxCocoa
+import RxSwift
+import SnapKit
+import Core
+import DesignSystem
+
+final class FeedViewController: UIViewController { }
