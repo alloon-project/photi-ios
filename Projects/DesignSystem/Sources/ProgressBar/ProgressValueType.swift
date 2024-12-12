7e2dd9e4d61c6d0634e8af93c2ed1418b216560b
Jung SeokYoung <thrdud0423@gmail.com>
2024-12-12 14:35:30 +0900

    [REFACT]: ProgressValueType에 double로 형변환할 수 있는 기능 추가

--- Projects/DesignSystem/Sources/ProgressBar/ProgressValueType.swift
+++ Projects/DesignSystem/Sources/ProgressBar/ProgressValueType.swift
@@ -15,11 +15,27 @@ public enum PhotiProgressStep {
   case five
 }
 /// Medium Type의 진행상태 값 분류입니다. 나타냅니다. (ex. percent0, percent20, ..., percent100)
-public enum PhotiProgressPercent {
-  case percent20
-  case percent40
-  case percent60
-  case percent80
-  case percent100
-  case percent0
+public enum PhotiProgressPercent: Double {
+  case percent0 = 0
+  case percent20 = 0.2
+  case percent40 = 0.4
+  case percent60 = 0.6
+  case percent80 = 0.8
+  case percent100 = 1
+  
+  public init(_ percent: Double) {
+    if percent < 0.1 {
+      self = .percent0
+    } else if percent < 0.3 {
+      self = .percent20
+    } else if percent < 0.5 {
+      self = .percent40
+    } else if percent < 0.7 {
+      self = .percent60
+    } else if percent < 0.9 {
+      self = .percent80
+    } else {
+      self = .percent100
+    }
+  }
 }
