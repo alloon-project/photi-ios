8d0a083663a8ae28b43362b4ec00d0867571746a
Jung SeokYoung <thrdud0423@gmail.com>
2024-12-12 09:47:16 +0900

    [FEAT]: FeeadPresentationModel 구현

---
+++ Projects/Presentation/Challenge/Implementations/Member/Feed/Views/FeedPresentationModel.swift
@@ -0,0 +1,16 @@
+//
+//  FeedPresentationModel.swift
+//  ChallengeImpl
+//
+//  Created by jung on 12/11/24.
+//  Copyright © 2024 com.photi. All rights reserved.
+//
+
+import Foundation
+
+struct FeedPresentationModel {
+  let id: String
+  let imageURL: String
+  let userName: String
+  let updateTime: String
+}
