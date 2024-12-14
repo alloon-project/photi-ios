2b5273c688ad1d570b0d9318866b7b5b98d2f504
Henry <wcbe9745@naver.com>
2024-12-14 20:22:21 +0900

    [FEAT]: EndedChallenge cell 구현

--- Projects/DesignSystem/Sources/Card/EndedChallengeCardCellModel.swift
+++ Projects/DesignSystem/Sources/Card/EndedChallengeCardCellModel.swift
@@ -8,10 +8,27 @@
 
 import UIKit
 
-struct EndedChallengeCardCellPresentationModel {
-  let challengeImage: UIImage
+public struct EndedChallengeCardCellPresentationModel {
+  let challengeImageUrl: URL?
   let challengeTitle: String
-  let finishedDate: Date
-  let challengeId: String
-  let challengeParticipantImages: [UIImage]
+  let endedDate: String
+  let challengeId: Int
+  let currentMemberCnt: Int
+  let challengeParticipantImageUrls: [URL?]
+  
+  public init(
+    challengeImageUrl: URL?,
+    challengeTitle: String,
+    endedDate: String,
+    challengeId: Int,
+    currentMemberCnt: Int,
+    challengeParticipantImageUrls: [URL?]
+  ) {
+    self.challengeImageUrl = challengeImageUrl
+    self.challengeTitle = challengeTitle
+    self.endedDate = endedDate
+    self.challengeId = challengeId
+    self.currentMemberCnt = currentMemberCnt
+    self.challengeParticipantImageUrls = challengeParticipantImageUrls
+  }
 }
