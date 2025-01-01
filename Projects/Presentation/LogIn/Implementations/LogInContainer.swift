ecba108f822d0522907acdd2006dd528f366bce2
Henry <wcbe9745@naver.com>
2025-01-01 21:26:55 +0900

    [FEAT]: Container 구현

--- Projects/Presentation/LogIn/Implementations/LogInContainer.swift
+++ Projects/Presentation/LogIn/Implementations/LogInContainer.swift
@@ -13,6 +13,7 @@ import UseCase
 public protocol LogInDependency: Dependency {
   var signUpContainable: SignUpContainable { get }
   var logInUseCase: LogInUseCase { get }
+  var findIdUseCase: FindIdUseCase { get }
   var findPasswordUseCase: FindPasswordUseCase { get }
 }
 
@@ -21,7 +22,9 @@ public final class LogInContainer:
   LogInContainable,
   FindIdDependency,
   FindPasswordDependency {
+  
   var loginUseCase: LogInUseCase { dependency.logInUseCase }
+  var findIdUseCase: FindIdUseCase { dependency.findIdUseCase }
   var findPasswordUseCase: FindPasswordUseCase { dependency.findPasswordUseCase }
 
   public func coordinator(listener: LogInListener) -> Coordinating {
