8aad24dbf74fd7d9e6c7f85720f19143f308b8d7
Jung SeokYoung <thrdud0423@gmail.com>
2025-01-09 14:33:46 +0900

    [REFACT]: Challenge에 변경된 Coordinator  적용

--- Projects/Presentation/Challenge/Implementations/Member/ChallengeContainer.swift
+++ Projects/Presentation/Challenge/Implementations/Member/ChallengeContainer.swift
@@ -15,13 +15,14 @@ public final class ChallengeContainer:
   Container<ChallengeDependency>,
   ChallengeContainable,
   FeedDependency {
-  public func coordinator(listener: ChallengeListener, challengeId: Int) -> Coordinating {
+  public func coordinator(listener: ChallengeListener, challengeId: Int) -> ViewableCoordinating {
+    let viewModel = ChallengeViewModel(challengeId: challengeId)
+    let viewControllerable = ChallengeViewController(viewModel: viewModel)
+    
     let feedContainer = FeedContainer(dependency: self)
     
-    let viewModel = ChallengeViewModel(challengeId: challengeId)
-    let viewController = ChallengeViewController(viewModel: viewModel)
     let coordinator = ChallengeCoordinator(
-      viewController: viewController,
+      viewControllerable: viewControllerable,
       viewModel: viewModel,
       feedContainer: feedContainer
     )
