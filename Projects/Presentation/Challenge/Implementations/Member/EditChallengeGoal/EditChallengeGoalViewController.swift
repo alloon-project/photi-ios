320349c0915c69f9206962ccf4097ee026f210e9
Jung SeokYoung <thrdud0423@gmail.com>
2025-01-21 11:12:37 +0900

    [FEAT]: 챌린지 목표 수정 UI 완료

--- Projects/Presentation/Challenge/Implementations/Member/EditChallengeGoal/EditChallengeGoalViewController.swift
+++ Projects/Presentation/Challenge/Implementations/Member/EditChallengeGoal/EditChallengeGoalViewController.swift
@@ -14,11 +14,40 @@ import Core
 import DesignSystem
 
 final class EditChallengeGoalViewController: UIViewController, ViewControllerable {
+  enum Constants {
+    static let navigationBarHeight = 56
+    static let mainTitleLabelText = "이 챌린지에서 이루고 싶은\n회원님만의 목표를 알려주세요"
+    static let subTitleLabelText = "다른 파티원도 확인할 수 있어요"
+  }
+  
   // MARK: - Properties
   private let viewModel: EditChallengeGoalViewModel
   private let disposeBag = DisposeBag()
   
   // MARK: - UI Components
+  private let navigationBar = PhotiNavigationBar(leftView: .backButton, displayMode: .dark)
+  private let imageView = UIImageView(image: .challengeEditGoal)
+  private let mainLabel: UILabel = {
+    let label = UILabel()
+    label.attributedText = Constants.mainTitleLabelText.attributedString(
+      font: .heading4,
+      color: .gray900
+    )
+    label.numberOfLines = 0
+    return label
+  }()
+  private let subLabel: UILabel = {
+    let label = UILabel()
+    label.attributedText = Constants.subTitleLabelText.attributedString(
+      font: .caption1,
+      color: .gray700
+    )
+    return label
+  }()
+  
+  private let textField = LineTextField(placeholder: "예) 잊지못할 추억 남기기", type: .count(16))
+  
+  private let saveButton = FilledRoundButton(type: .primary, size: .xLarge, text: "내 목표 저장하기")
   
   // MARK: - Initializers
   init(viewModel: EditChallengeGoalViewModel) {
@@ -46,9 +75,50 @@ private extension EditChallengeGoalViewController {
     setConstraints()
   }
   
-  func setViewHierarchy() { }
+  func setViewHierarchy() {
+    view.addSubviews(
+      navigationBar,
+      imageView,
+      mainLabel,
+      subLabel,
+      textField,
+      saveButton
+    )
+  }
   
-  func setConstraints() { }
+  func setConstraints() {
+    navigationBar.snp.makeConstraints {
+      $0.leading.trailing.equalToSuperview()
+      $0.top.equalTo(view.safeAreaLayoutGuide)
+      $0.height.equalTo(Constants.navigationBarHeight)
+    }
+    
+    imageView.snp.makeConstraints {
+      $0.leading.trailing.equalToSuperview().inset(24)
+      $0.top.equalTo(navigationBar.snp.bottom).offset(24)
+      $0.height.equalTo(126)
+    }
+    
+    mainLabel.snp.makeConstraints {
+      $0.leading.trailing.equalToSuperview().inset(24)
+      $0.top.equalTo(imageView.snp.bottom).offset(24)
+    }
+    
+    subLabel.snp.makeConstraints {
+      $0.leading.trailing.equalToSuperview().inset(24)
+      $0.top.equalTo(mainLabel.snp.bottom).offset(16)
+    }
+    
+    textField.snp.makeConstraints {
+      $0.leading.trailing.equalToSuperview().inset(24)
+      $0.top.equalTo(subLabel.snp.bottom).offset(24)
+    }
+    
+    saveButton.snp.makeConstraints {
+      $0.leading.trailing.equalToSuperview().inset(24)
+      $0.bottom.equalToSuperview().inset(48)
+    }
+  }
 }
 
 // MARK: - Bind Methods
