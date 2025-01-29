b5875c287c84a13cb37f0ced66683ae99c1c8534
Jung SeokYoung <thrdud0423@gmail.com>
2025-01-30 02:22:00 +0900

    [REFACT]: ChallengeDataMapper에서 EndedChallenge까지 기능하도록 수정 및 Entity 변경에 따른 수정

--- Projects/Data/DataMapper/ChallengeDataMapper.swift
+++ Projects/Data/DataMapper/ChallengeDataMapper.swift
@@ -12,25 +12,48 @@ import Core
 import Entity
 
 public protocol ChallengeDataMapper {
-  func mapToChallenge(dto: ChallengeResponseDTO) -> Challenge
+  func mapToChallengeDetail(dto: PopularChallengeResponseDTO) -> ChallengeDetail
+  func mapToChallengeSummary(dto: EndedChallengeResponseDTO) -> [ChallengeSummary]
 }
 
 public struct ChallengeDataMapperImpl: ChallengeDataMapper {
   public init() {}
   
-  public func mapToChallenge(dto: ChallengeResponseDTO) -> Challenge {
+  public func mapToChallengeDetail(dto: PopularChallengeResponseDTO) -> ChallengeDetail {
     let endDate = dto.endDate.toDate() ?? Date()
-    let proveTime = dto.proveTime.toDate("HH:mm") ?? Date()
     let hasTags = dto.hashtags.map { $0.hashtag }
+    let proveTime = dto.proveTime.toDate("HH:mm") ?? Date()
+    let memberImages = dto.memberImages.map { $0.memberImage }
     
     return .init(
       id: dto.id,
       name: dto.name,
-      imageURL: dto.imageUrl,
-      goal: dto.goal,
-      proveTime: proveTime,
+      imageUrl: dto.imageUrl,
       endDate: endDate,
-      hashTags: hasTags
+      hashTags: hasTags,
+      proveTime: proveTime,
+      goal: dto.goal,
+      memberCount: dto.currentMemberCnt,
+      memberImages: memberImages
     )
+  }
+  
+  public func mapToChallengeSummary(dto: EndedChallengeResponseDTO) -> [ChallengeSummary] {
+    let contents = dto.content
+    
+    return dto.content.map {
+      let endDate = $0.endDate.toDate() ?? Date()
+      let memberImages = $0.memberImages.map { $0.memberImage }
+      
+      return .init(
+        id: $0.id,
+        name: $0.name,
+        imageUrl: $0.imageUrl,
+        endDate: endDate,
+        hashTags: [],
+        memberCount: $0.currentMemberCnt,
+        memberImages: memberImages
+      )
+    }
   }
 }
