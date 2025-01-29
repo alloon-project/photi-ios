f99dfda5431d99f7b33a3579136b2c2c16cec438
Jung SeokYoung <thrdud0423@gmail.com>
2025-01-30 02:22:52 +0900

    [REFACT]: ChallengeAPI에서 EndedChallenge까지 조회가능하도록 수정

--- Projects/Data/Repository/Implementations/ChallengeRepositoryImpl/ChallengeAPI.swift
+++ Projects/Data/Repository/Implementations/ChallengeRepositoryImpl/ChallengeAPI.swift
@@ -13,23 +13,29 @@ import PhotiNetwork
 
 public enum ChallengeAPI {
   case popularChallenges
+  case challengeDetail(id: Int)
+  case endedChallenges(page: Int, size: Int)
 }
 
 extension ChallengeAPI: TargetType {
   public var baseURL: URL {
     //    return ServiceConfiguration.baseUrl
-    return URL(string: "http://localhost:8080/challenges")!
+    return URL(string: "http://localhost:8080/api")!
   }
   
   public var path: String {
     switch self {
-      case .popularChallenges: return "popular"
+      case .popularChallenges: return "challenges/popular"
+      case let .challengeDetail(id): return "challenges/\(id)"
+      case .endedChallenges: return "users/ended-challenges"
     }
   }
   
   public var method: HTTPMethod {
     switch self {
       case .popularChallenges: return .get
+      case .challengeDetail: return .get
+      case .endedChallenges: return .get
     }
   }
   
@@ -37,13 +43,30 @@ extension ChallengeAPI: TargetType {
     switch self {
       case .popularChallenges:
         return .requestPlain
+      case .challengeDetail:
+        return .requestPlain
+      case let .endedChallenges(page, size):
+        let parameters = ["page": page, "size": size]
+        return .requestParameters(parameters: parameters, encoding: URLEncoding(destination: .queryString))
     }
   }
   
   public var sampleResponse: EndpointSampleResponse {
     switch self {
       case .popularChallenges:
-        let data = ChallengeResponseDTO.stubData
+        let data = PopularChallengeResponseDTO.stubData
+        let jsonData = data.data(using: .utf8)
+        
+        return .networkResponse(200, jsonData ?? Data(), "OK", "성공")
+        
+      case .challengeDetail:
+        let data = ChallengeDetailResponseDTO.stubData
+        let jsonData = data.data(using: .utf8)
+        
+        return .networkResponse(200, jsonData ?? Data(), "OK", "성공")
+        
+      case .endedChallenges:
+        let data = EndedChallengeResponseDTO.stubData
         let jsonData = data.data(using: .utf8)
         
         return .networkResponse(200, jsonData ?? Data(), "OK", "성공")
