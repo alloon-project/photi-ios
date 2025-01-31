c5b35d2c96ab4950d8c6e523cc93967a8bb4ef50
Jung SeokYoung <thrdud0423@gmail.com>
2025-01-31 10:13:03 +0900

    [FEAT]: ChallengeDetailResponseDTO에 빠져있던 프로퍼티 추가

--- Projects/Data/DTO/Challenge/ChallengeDetail/ChallengeDetailResponseDTO.swift
+++ Projects/Data/DTO/Challenge/ChallengeDetail/ChallengeDetailResponseDTO.swift
@@ -10,8 +10,9 @@ import Foundation
 
 public struct ChallengeDetailResponseDTO: Decodable {
   public let name: String
+  public let goal: String
   public let imageUrl: URL?
-  public let currentNumberCnt: Int
+  public let currentMemberCnt: Int
   public let isPublic: Bool
   public let proveTime: String
   public let endDate: String
@@ -22,27 +23,48 @@ public struct ChallengeDetailResponseDTO: Decodable {
 
 public extension ChallengeDetailResponseDTO {
   static let stubData = """
-  {
-    "code": "200 OK",
-    "message": "성공",
-    "data": [
+{
+  "code": "200 OK",
+  "message": "성공",
+  "data": {
+    "name": "신나게 하는 러닝 챌린지",
+    "goal": "하루에 한 번씩 꼭 러닝을 하는 것이 우리 챌린지의 목표입니다.",
+    "imageUrl": "https://url.kr/5MhHhD",
+    "currentMemberCnt": 5,
+    "isPublic": true,
+    "proveTime": "13:00",
+    "endDate": "2024-12-01",
+    "rules": [
       {
-        "id": 1,
-        "name": "신나게 하는 러닝 챌린지",
-        "imageUrl": "https://url.kr/5MhHhD",
-        "goal": "하루에 한 번씩 꼭 러닝을 하는 것이 우리 챌린지의 목표입니다.",
-        "proveTime": "13:00",
-        "endDate": "2024-12-01",
-        "hashtags": [
-          {
-            "hashtag": "러닝"
-          },
-          {
-            "hashtag": "건강"
-          }
-        ]
+        "rule": "장소 나오게 찍기"
+      },
+      {
+        "rule": "일주일에 3회 이상 인증하기"
+      },
+      {
+        "rule": "얼굴 안 나오게 찍기"
+      }
+    ],
+    "hashtags": [
+      {
+        "hashtag": "러닝"
+      },
+      {
+        "hashtag": "건강"
+      }
+    ],
+    "memberImages": [
+      {
+        "memberImage": "https://url.kr/5MhHhD"
+      },
+      {
+        "memberImage": "https://url.kr/5MhHhD"
+      },
+      {
+        "memberImage": "https://url.kr/5MhHhD"
       }
     ]
   }
+}
 """
 }
