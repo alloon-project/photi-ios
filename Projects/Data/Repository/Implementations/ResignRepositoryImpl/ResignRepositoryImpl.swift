e01d27b444d0c756f207ae9cd49689f56e230b7f
Henry <wcbe9745@naver.com>
2025-02-18 22:40:03 +0900

    [FEAT]: Resign Repository구현

---
+++ Projects/Data/Repository/Implementations/ResignRepositoryImpl/ResignRepositoryImpl.swift
@@ -0,0 +1,49 @@
+//
+//  ResignRepositoryImpl.swift
+//  Data
+//
+//  Created by 임우섭 on 2/16/25.
+//  Copyright © 2025 com.photi. All rights reserved.
+//
+
+import RxSwift
+import DataMapper
+import Entity
+import Repository
+import PhotiNetwork
+
+public struct ResignRepositoryImpl: ResignRepository {
+  private let dataMapper: ResignDataMapper
+  
+  public init(dataMapper: ResignDataMapper) {
+    self.dataMapper = dataMapper
+  }
+  
+  public func resign(
+    password: String
+  ) -> Single<Void> {
+    let requestDTO = dataMapper.mapToResignRequestDTO(userPassword: password)
+    return Single.create { single in
+      Task {
+        do {
+          let result = try await Provider(stubBehavior: .never)
+            .request(ResignAPI.resign(dto: requestDTO)).value
+          
+          if result.statusCode == 200 {
+            single(.success(()))
+          } else if result.statusCode == 400 {
+            single(.failure(APIError.passwordMatchInvalid))
+          } else if result.statusCode == 401 {
+            single(.failure(APIError.loginUnauthenticated))
+          } else if result.statusCode == 403 {
+            single(.failure(APIError.tokenUnauthorized))
+          }
+        } catch {
+          single(.failure(error))
+        }
+      }
+      
+      return Disposables.create()
+    }
+  }
+}
