98d3d13daa1e5dea346b500c603bd6723bde416c
Henry <wcbe9745@naver.com>
2025-02-23 16:08:39 +0900

    [FEAT]: API 구현

---
+++ Projects/Data/Repository/Implementations/FeedRepositoryImpl/FeedAPI.swift
@@ -0,0 +1,53 @@
+//
+//  FeedAPI.swift
+//  Data
+//
+//  Created by 임우섭 on 2/23/25.
+//  Copyright © 2025 com.photi. All rights reserved.
+//
+
+import Foundation
+import Core
+import DTO
+import PhotiNetwork
+
+public enum FeedAPI {
+  case feedHistory(page: Int, size: Int)
+}
+
+extension FeedAPI: TargetType {
+  public var baseURL: URL {
+    //    return ServiceConfiguration.baseUrl
+    return URL(string: "http://localhost:8080/api")!
+  }
+  
+  public var path: String {
+    switch self {
+    case .feedHistory: return "users/feed-history"
+    }
+  }
+  
+  public var method: HTTPMethod {
+    switch self {
+    case .feedHistory: return .get
+    }
+  }
+  
+  public var task: TaskType {
+    switch self {
+    case let .feedHistory(page, size):
+      let parameters = ["page": page, "size": size]
+      return .requestParameters(parameters: parameters, encoding: URLEncoding(destination: .queryString))
+    }
+  }
+  
+  public var sampleResponse: EndpointSampleResponse {
+    switch self {
+    case .feedHistory:
+      let data = FeedHistoryResponseDTO.stubData
+        let jsonData = data.data(using: .utf8)
+        
+        return .networkResponse(200, jsonData ?? Data(), "OK", "성공")
+    }
+  }
+}
