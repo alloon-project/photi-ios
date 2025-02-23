4b60a87932af50d59e12571005cd135a0e8cef39
Henry <wcbe9745@naver.com>
2025-02-23 16:09:19 +0900

    [FEAT]: UseCase 구현

---
+++ Projects/Domain/UseCase/Implementations/FeedUseCaseImpl.swift
@@ -0,0 +1,24 @@
+//
+//  FeedUseCaseImpl.swift
+//  Domain
+//
+//  Created by 임우섭 on 2/23/25.
+//  Copyright © 2025 com.photi. All rights reserved.
+//
+
+import RxSwift
+import Entity
+import UseCase
+import Repository
+
+public struct FeedUseCaseImpl: FeedUseCase {
+  private let repository: FeedRepository
+  
+  public init(repository: FeedRepository) {
+    self.repository = repository
+  }
+  
+  public func fetchFeeds(page: Int, size: Int) -> Single<[FeedHistory]> {
+    return repository.fetchFeedHistory(page: page, size: size)
+  }
+}
