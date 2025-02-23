548c3e35ef52189a18c1cd416b2fe857a4190a9f
Jung SeokYoung <thrdud0423@gmail.com>
2025-02-24 00:43:54 +0900

    [REFACT]: FeedPresentationModel 수정

---
+++ Projects/Presentation/Challenge/Implementations/Member/Feed/PresentationModel/FeedPresentationModel.swift
@@ -0,0 +1,42 @@
+//
+//  FeedPresentationModel.swift
+//  ChallengeImpl
+//
+//  Created by jung on 12/11/24.
+//  Copyright © 2024 com.photi. All rights reserved.
+//
+
+import Foundation
+
+struct FeedPresentationModel: Hashable {
+  let id: Int
+  let imageURL: URL
+  let userName: String
+  let updateTime: String 
+  let isLike: Bool
+}
+
+enum FeedAlignMode: String, CaseIterable {
+  case recent = "최신순"
+  case popular = "인기순"
+}
+
+enum ProveType: Equatable {
+  case didProof
+  case didNotProof(String)
+  
+  var toString: String {
+    switch self {
+      case .didProof:
+        return "인증 완료"
+      case let .didNotProof(time):
+        return "\(time)까지"
+    }
+  }
+}
+
+enum FeedsType {
+  case initialPage([FeedPresentationModel])
+  case `default`([FeedPresentationModel])
+  case lastPage([FeedPresentationModel])
+}
