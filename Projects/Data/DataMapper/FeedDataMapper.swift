322e76b407ff0c6618fdc33efb1d992cb6554708
Henry <wcbe9745@naver.com>
2025-02-23 16:08:04 +0900

    [FEAT]: DataMapper구현

---
+++ Projects/Data/DataMapper/FeedDataMapper.swift
@@ -0,0 +1,34 @@
+//
+//  FeedDataMapper.swift
+//  Data
+//
+//  Created by 임우섭 on 2/23/25.
+//  Copyright © 2025 com.photi. All rights reserved.
+//
+
+import Foundation
+import DTO
+import Core
+import Entity
+
+public protocol FeedDataMapper {
+  func mapToFeedHistory(dto: FeedHistoryResponseDTO) -> [FeedSummary]
+}
+
+public struct FeedDataMapperImpl: FeedDataMapper {
+  public init() {}
+  
+  public func mapToFeedHistory(dto: FeedHistoryResponseDTO) -> [FeedSummary] {
+    let contents = dto.content
+    
+    return dto.content.map {
+      return .init(
+        feedId: $0.feedId,
+        challengeId: $0.challengeId,
+        imageUrl: $0.imageUrl,
+        createDate: $0.createDate,
+        name: $0.name
+      )
+    }
+  }
+}
