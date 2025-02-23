8e43c764973ef9c92bef2676b525aea37a850349
Henry <wcbe9745@naver.com>
2025-02-23 16:07:39 +0900

    [FEAT]: Entity 구현

---
+++ Projects/Domain/Entity/FeedHistory.swift
@@ -0,0 +1,34 @@
+//
+//  FeedHistory.swift
+//  Domain
+//
+//  Created by 임우섭 on 2/23/25.
+//  Copyright © 2025 com.photi. All rights reserved.
+//
+
+import Foundation
+
+public struct FeedHistory {
+  public let feedId: Int
+  public let challengeId: String
+  public let imageUrl: URL?
+  public let createDate: Date
+  public let name: String
+  
+  public init(
+    feedId: Int,
+    challengeId: String,
+    imageUrl: URL?,
+    createDate: Date,
+    name: String
+  ) {
+    self.feedId = feedId
+    self.challengeId = challengeId
+    self.imageUrl = imageUrl
+    self.createDate = createDate
+    self.name = name
+  }
+}
+
+// MARK: - Initializers
+public extension FeedHistory {}
