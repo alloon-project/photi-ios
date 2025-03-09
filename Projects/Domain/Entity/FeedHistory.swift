dc1e3f058b1ff44dc3ca728d09e0e754ff9f6fb8
Henry <wcbe9745@naver.com>
2025-03-09 14:50:11 +0900

    REFACT: FeedHistory 타입 수정

--- Projects/Domain/Entity/FeedHistory.swift
+++ Projects/Domain/Entity/FeedHistory.swift
@@ -10,14 +10,14 @@ import Foundation
 
 public struct FeedHistory {
   public let feedId: Int
-  public let challengeId: String
+  public let challengeId: Int
   public let imageUrl: URL?
   public let provedDate: Date
   public let name: String
   
   public init(
     feedId: Int,
-    challengeId: String,
+    challengeId: Int,
     imageUrl: URL?,
     createDate: Date,
     name: String
