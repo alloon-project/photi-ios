bfc9d93182c5d807ef96698abb58d5eddf386b82
Jung SeokYoung <thrdud0423@gmail.com>
2025-03-13 15:26:33 +0900

    [FEAT]: Description ViewModel 연결

--- Projects/Presentation/Challenge/Implementations/Member/Description/DescriptionViewModel.swift
+++ Projects/Presentation/Challenge/Implementations/Member/Description/DescriptionViewModel.swift
@@ -6,7 +6,9 @@
 //  Copyright © 2025 com.photi. All rights reserved.
 //
 
+import RxCocoa
 import RxSwift
+import Entity
 import UseCase
 
 protocol DescriptionCoordinatable: AnyObject { }
@@ -23,12 +25,22 @@ final class DescriptionViewModel: DescriptionViewModelType {
   private let challengeId: Int
   private let useCase: ChallengeUseCase
   private let disposeBag = DisposeBag()
+  
+  private let description = BehaviorRelay<ChallengeDescription?>(value: nil)
+  private let descriptionObservable: Observable<ChallengeDescription>
 
   // MARK: - Input
-  struct Input { }
+  struct Input {
+    let requestData: Signal<Void>
+  }
   
   // MARK: - Output
-  struct Output { }
+  struct Output {
+    let rules: Driver<[String]>
+    let proveTime: Driver<String>
+    let goal: Driver<String>
+    let duration: Driver<String>
+  }
   
   // MARK: - Initializers
   init(challengeId: Int, useCase: ChallengeUseCase) {
@@ -38,6 +50,36 @@ final class DescriptionViewModel: DescriptionViewModelType {
   }
   
   func transform(input: Input) -> Output {
-    return Output()
+    input.requestData
+      .emit(with: self) { owner, _ in
+        owner.fetchDescription()
+      }
+      .disposed(by: disposeBag)
+    
+    let proveTime = descriptionObservable.map { $0.proveTime.toString("HH : mm") }
+    let duration = descriptionObservable.map {
+      "\($0.startDate.toString("yyyy.MM.dd")) ~ \($0.endDate.toString("yyyy-MM-dd"))"
+    }
+    
+    return Output(
+      rules: descriptionObservable.map { $0.rules }.asDriver(onErrorJustReturn: []),
+      proveTime: proveTime.asDriver(onErrorJustReturn: ""),
+      goal: descriptionObservable.map { $0.goal }.asDriver(onErrorJustReturn: ""),
+      duration: duration.asDriver(onErrorJustReturn: "")
+    )
+  }
+}
+
+// MARK: - Private Methods
+private extension DescriptionViewModel {
+  func fetchDescription() {
+    useCase.fetchChallengeDescription(id: challengeId)
+      .observe(on: MainScheduler.instance)
+      .subscribe(with: self) { owner, description in
+        owner.description.accept(description)
+      } onFailure: { _, _ in
+        print("error")
+      }
+      .disposed(by: disposeBag)
   }
 }
