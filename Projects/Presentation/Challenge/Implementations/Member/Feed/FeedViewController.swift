8425901b6d66bd263610f3e7638599f47cdb9c19
Jung SeokYoung <thrdud0423@gmail.com>
2025-04-14 00:50:47 +0900

    [REFACT]: FeedViewController UI 수정

--- Projects/Presentation/Challenge/Implementations/Member/Feed/FeedViewController.swift
+++ Projects/Presentation/Challenge/Implementations/Member/Feed/FeedViewController.swift
@@ -14,6 +14,13 @@ import Core
 import DesignSystem
 
 final class FeedViewController: UIViewController, ViewControllerable, CameraRequestable {
+  enum Constants {
+    static let feedSpacing = 7.0
+    static let feedLineSpacing = 10.0
+    static let feedHeaderHeight = 44.0
+    static let feedFooterHeight = 6.0
+  }
+  
   typealias DataSourceType = UICollectionViewDiffableDataSource<String, FeedPresentationModel>
   typealias SnapShot = NSDiffableDataSourceSnapshot<String, FeedPresentationModel>
 
@@ -32,7 +39,8 @@ final class FeedViewController: UIViewController, ViewControllerable, CameraRequ
     didSet {
       guard viewWillAppear else { return }
       configureTodayHeaderView(for: isProve)
-      cameraShutterButton.isHidden = (isProve != .didProve)
+      cameraShutterButton.isHidden = (isProve == .didProve)
+      if isProve != .didProve { presentPoofTipView() }
     }
   }
   private var feedsAlign: FeedsAlignMode = .recent {
@@ -56,17 +64,7 @@ final class FeedViewController: UIViewController, ViewControllerable, CameraRequ
   private let orderButton = IconTextButton(text: "최신순", icon: .chevronDownGray700, size: .xSmall)
   private let tagView = TagView(image: .peopleWhite)
   private let feedCollectionView: SelfVerticalSizingCollectionView = {
-    let layout = UICollectionViewFlowLayout()
-    layout.scrollDirection = .vertical
-    layout.minimumLineSpacing = 10
-    layout.minimumInteritemSpacing = 7
-    layout.headerReferenceSize = .init(width: 0, height: 44)
-    layout.footerReferenceSize = .init(width: 0, height: 6)
-    layout.sectionInset = .init(top: 8, left: 0, bottom: 18, right: 0)
-    layout.itemSize = .init(width: 160, height: 160)
-    layout.sectionHeadersPinToVisibleBounds = true
-    
-    let collectionView = SelfVerticalSizingCollectionView(layout: layout)
+    let collectionView = SelfVerticalSizingCollectionView(layout: UICollectionViewLayout())
     collectionView.registerCell(FeedCell.self)
     collectionView.registerHeader(FeedsHeaderView.self)
     collectionView.registerFooter(FeedsLoadingFooterView.self)
@@ -103,6 +101,7 @@ final class FeedViewController: UIViewController, ViewControllerable, CameraRequ
     let dataSource = diffableDataSource()
     self.dataSource = dataSource
     feedCollectionView.dataSource = dataSource
+    feedCollectionView.collectionViewLayout = compositionalLayout()
     dataSource.supplementaryViewProvider = supplementaryViewProvider()
     feedCollectionView.delegate = self
     configureRefreshControl()
@@ -113,6 +112,7 @@ final class FeedViewController: UIViewController, ViewControllerable, CameraRequ
     super.viewWillAppear(animated)
     guard !viewWillAppear else { return }
     self.viewWillAppear = true
+    if case let .didNotProve(time) = isProve, time.isEmpty { return }
     cameraShutterButton.isHidden = (isProve == .didProve)
     if isProve != .didProve { presentPoofTipView() }
   }
@@ -163,8 +163,8 @@ private extension FeedViewController {
     
     feedCollectionView.snp.makeConstraints {
       $0.top.equalTo(orderButton.snp.bottom).offset(30)
-      $0.centerX.bottom.equalToSuperview()
-      $0.width.equalTo(327)
+      $0.leading.trailing.equalToSuperview().inset(24)
+      $0.bottom.equalToSuperview()
     }
 
     cameraShutterButton.snp.makeConstraints {
@@ -212,9 +212,9 @@ private extension FeedViewController {
     
     output.feeds
       .drive(with: self) { owner, feeds in
+        owner.feedCollectionView.refreshControl?.endRefreshing()
         switch feeds {
           case let .initialPage(models):
-            owner.feedCollectionView.refreshControl?.endRefreshing()
             owner.deleteAllFeeds()
             owner.initialize(models: models)
           case let .default(models):
@@ -439,6 +439,50 @@ extension FeedViewController: AlignBottomSheetDelegate {
 
 // MARK: - Private Methods
 private extension FeedViewController {
+  func compositionalLayout() -> UICollectionViewCompositionalLayout {
+    return .init { _, environment in
+      let itemSpacing = Constants.feedSpacing
+      let availableWidth = environment.container.effectiveContentSize.width
+      let itemWidth = (availableWidth - itemSpacing) / 2
+      
+      let itemSize = NSCollectionLayoutSize(widthDimension: .absolute(itemWidth), heightDimension: .absolute(itemWidth))
+      let item = NSCollectionLayoutItem(layoutSize: itemSize)
+      
+      let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .absolute(itemWidth))
+      let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item, item])
+      group.interItemSpacing = .fixed(itemSpacing)
+      
+      let section = NSCollectionLayoutSection(group: group)
+      section.interGroupSpacing = 10
+      section.contentInsets = NSDirectionalEdgeInsets(top: 8, leading: 0, bottom: 18, trailing: 0)
+      
+      let headerSize = NSCollectionLayoutSize(
+        widthDimension: .fractionalWidth(1.0),
+        heightDimension: .absolute(Constants.feedHeaderHeight)
+      )
+      
+      let header = NSCollectionLayoutBoundarySupplementaryItem(
+        layoutSize: headerSize,
+        elementKind: UICollectionView.elementKindSectionHeader,
+        alignment: .top
+      )
+      header.pinToVisibleBounds = true
+      
+      let footerSize = NSCollectionLayoutSize(
+        widthDimension: .fractionalWidth(1.0),
+        heightDimension: .absolute(Constants.feedFooterHeight)
+      )
+      let footer = NSCollectionLayoutBoundarySupplementaryItem(
+        layoutSize: footerSize,
+        elementKind: UICollectionView.elementKindSectionFooter,
+        alignment: .bottom
+      )
+
+      section.boundarySupplementaryItems = [header, footer]
+      return section
+    }
+  }
+  
   func configureRefreshControl() {
     feedCollectionView.refreshControl = UIRefreshControl()
     feedCollectionView.refreshControl?.addTarget(self, action: #selector(refreshStart), for: .valueChanged)
