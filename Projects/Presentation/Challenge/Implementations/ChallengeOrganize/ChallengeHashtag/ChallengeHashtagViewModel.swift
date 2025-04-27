d0e7b49e9155dc34d9dd995fd9e0d4f08fb6a19a
Henry <wcbe9745@naver.com>
2025-04-27 19:41:41 +0900

    [FEAT]: ChallengeHashtag ViewModel 구현

---
+++ Projects/Presentation/Challenge/Implementations/ChallengeOrganize/ChallengeHashtag/ChallengeHashtagViewModel.swift
@@ -0,0 +1,66 @@
+//
+//  ChallengeHashtagViewModel.swift
+//  Presentation
+//
+//  Created by 임우섭 on 3/16/25.
+//  Copyright © 2025 com.photi. All rights reserved.
+//
+
+import RxCocoa
+import RxSwift
+import UseCase
+
+protocol ChallengeHashtagCoordinatable: AnyObject {
+  func didTapBackButtonAtChallengeHashtag()
+  func didFinishedAtChallengeHashtag(challengeHashtags: [String])
+}
+
+protocol ChallengeHashtagViewModelType: AnyObject {
+  associatedtype Input
+  associatedtype Output
+  
+  var disposeBag: DisposeBag { get }
+  var coordinator: ChallengeHashtagCoordinatable? { get set }
+}
+
+final class ChallengeHashtagViewModel: ChallengeHashtagViewModelType {
+  let disposeBag = DisposeBag()
+  
+  weak var coordinator: ChallengeHashtagCoordinatable?
+      
+  // MARK: - Input
+  struct Input {
+    let didTapBackButton: ControlEvent<Void>
+    let enteredHashtag: ControlProperty<String>
+    let selectedHashtags: Observable<[String]>
+    let didTapNextButton: ControlEvent<Void>
+  }
+  
+  // MARK: - Output
+  struct Output {
+    let isValidHashtag: Driver<Bool>
+  }
+  
+  // MARK: - Initializers
+  init() {}
+  
+  func transform(input: Input) -> Output {
+    input.didTapBackButton
+      .bind(with: self) { owner, _ in
+        owner.coordinator?.didTapBackButtonAtChallengeHashtag()
+      }
+      .disposed(by: disposeBag)
+    
+    let isHashtagEntered = input.enteredHashtag.map { !$0.isEmpty && $0.count <= 6 }
+    
+    input.didTapNextButton
+      .withLatestFrom(input.selectedHashtags)
+      .bind(with: self) { owner, hashtags in
+        owner.coordinator?.didFinishedAtChallengeHashtag(challengeHashtags: hashtags)
+      }.disposed(by: disposeBag)
+    
+    return Output(
+      isValidHashtag: isHashtagEntered.asDriver(onErrorJustReturn: false)
+    )
+  }
+}
