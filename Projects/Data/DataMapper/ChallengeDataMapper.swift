012cbe786636395fc2ddf508b1eada459c25f257
Henry <wcbe9745@naver.com>
2025-05-04 17:41:37 +0900

    [REFACT]: Organize DataMapper 분리작업

--- Projects/Data/DataMapper/ChallengeDataMapper.swift
+++ Projects/Data/DataMapper/ChallengeDataMapper.swift
@@ -22,18 +22,6 @@ public protocol ChallengeDataMapper {
   func mapToChallengeDescription(dto: ChallengeDescriptionResponseDTO, id: Int) -> ChallengeDescription
   func mapToChallengeMembers(dto: [ChallengeMemberResponseDTO]) -> [ChallengeMember]
   func mapToFeedHistory(dto: FeedHistoryResponseDTO) -> [FeedHistory]
-  func mapToSampleImages(dto: ChallengeSampleImageResponseDTO) -> [String]
-  func mapToOrganizedChallenge(
-    name: String,
-    isPublic: Bool,
-    goal: String,
-    proveTime: String,
-    endDate: String,
-    rules: [[String: String]],
-    hashtags: [[String: String]],
-    image: Data,
-    imageType: String
-  ) -> ChallengeOrganizeRequestDTO
 }
 
 public struct ChallengeDataMapperImpl: ChallengeDataMapper {
@@ -185,33 +173,5 @@ public struct ChallengeDataMapperImpl: ChallengeDataMapper {
         name: $0.name
       )
     }
-  }
-  
-  public func mapToSampleImages(dto: ChallengeSampleImageResponseDTO) -> [String] {
-    return dto.list.map { $0 }
-  }
-  
-  public func mapToOrganizedChallenge(
-    name: String,
-    isPublic: Bool,
-    goal: String,
-    proveTime: String,
-    endDate: String,
-    rules: [[String: String]],
-    hashtags: [[String: String]],
-    image: Data,
-    imageType: String
-  ) -> ChallengeOrganizeRequestDTO {
-    return ChallengeOrganizeRequestDTO(
-      name: name,
-      isPublic: isPublic,
-      goal: goal,
-      proveTime: proveTime,
-      endDate: endDate,
-      rules: rules,
-      hashtags: hashtags,
-      image: image,
-      imageType: imageType
-    )
   }
 }
---
+++ Projects/Data/DataMapper/ChallengeOrganizeDataMapper.swift
@@ -0,0 +1,37 @@
+//
+//  ChallengeOrganizeDataMapper.swift
+//  Data
+//
+//  Created by 임우섭 on 5/4/25.
+//  Copyright © 2025 com.photi. All rights reserved.
+//
+
+import DTO
+import Entity
+
+public protocol ChallengeOrganizeDataMapper {
+  func mapToSampleImages(dto: ChallengeSampleImageResponseDTO) -> [String]
+  func mapToOrganizedChallenge(payload: ChallengeOrganizePayload)
+  -> ChallengeOrganizeRequestDTO
+}
+
+public struct ChallengeOrganizeDataMapperImpl: ChallengeOrganizeDataMapper {
+  public func mapToSampleImages(dto: ChallengeSampleImageResponseDTO) -> [String] {
+    return dto.list.map { $0 }
+  }
+  
+  public func mapToOrganizedChallenge(payload: ChallengeOrganizePayload)
+  -> ChallengeOrganizeRequestDTO {
+    return ChallengeOrganizeRequestDTO(
+      name: payload.name,
+      isPublic: payload.isPublic,
+      goal: payload.goal,
+      proveTime: payload.proveTime,
+      endDate: payload.endDate,
+      rules: payload.rules,
+      hashtags: payload.hashtags,
+      image: payload.image,
+      imageType: payload.imageType
+    )
+  }
+}
