584d93e77b019c45a4378deee919c92a34612561
Jung SeokYoung <thrdud0423@gmail.com>
2025-05-19 15:59:35 +0900

    [REFACT]: App 최상단에서 로그인 관련 라우팅하도록 적용

--- Projects/App/Sources/AppCoordinator.swift
+++ Projects/App/Sources/AppCoordinator.swift
@@ -24,9 +24,15 @@ final class AppCoordinator: ViewableCoordinator<AppPresentable> {
   private let searchChallengeNavigationControllerable = NavigationControllerable()
   private let myPageNavigationControllerable = NavigationControllerable()
   
-  private let homeContainable: HomeContainable
-  private let searchChallengeContainable: SearchChallengeContainable
-  private let myPageContainable: MyPageContainable
+  private let homeContainer: HomeContainable
+  private var homeCoorinator: Coordinating?
+  
+  private let searchChallengeContainer: SearchChallengeContainable
+  private var searchChallengeCoordinator: ViewableCoordinating?
+  
+  private let myPageContainer: MyPageContainable
+  private var myPageCoordinator: ViewableCoordinating?
+  
   private let loginContainer: LogInContainable
   private var loginCoordinator: ViewableCoordinating?
   private var loginPresentNavigationControllerable: NavigationControllerable?
@@ -46,34 +52,122 @@ final class AppCoordinator: ViewableCoordinator<AppPresentable> {
   }
   
   override func start() {
-    attachCoordinators()
+    Task { await attachCoordinators() }
   }
-  
-  func attachCoordinators() {
-    let homeCoordinator = homeContainable.coordinator(
-      navigationControllerable: homeNavigationControllerable,
-      listener: self
-    )
-    let searchChallengeCoordinator = searchChallengeContainable.coordinator(listener: self)
-    let myPageCoordinator = myPageContainable.coordinator(listener: self)
+}
+
+// MARK: - LogIn
+private extension AppCoordinator {
+  @MainActor func attachLogIn(at navigationController: NavigationControllerable) {
+    guard loginCoordinator == nil else { return }
+
+    let coordinator = loginContainer.coordinator(listener: self)
+    addChild(coordinator)
+    navigationController.navigationController.hideTabBar(animated: true)
+    navigationController.pushViewController(coordinator.viewControllerable, animated: true)
+    self.loginCoordinator = coordinator
+    self.loginPresentNavigationControllerable = navigationController
+  }
+
+  @MainActor func detachLogIn(willPopViewConroller: Bool) {
+    guard
+      let coordinator = loginCoordinator,
+      let navigation = loginPresentNavigationControllerable
+    else { return }
+
+    removeChild(coordinator)
+    self.loginCoordinator = nil
+    self.loginPresentNavigationControllerable = nil
     
-    searchChallengeNavigationControllerable.setViewControllers([searchChallengeCoordinator.viewControllerable])
-    myPageNavigationControllerable.setViewControllers([myPageCoordinator.viewControllerable])
-        
+    navigation.navigationController.showTabBar(animted: true)
+    if willPopViewConroller { navigation.popViewController(animated: true) }
+  }
+}
+
+// MARK: - Attach Methods
+private extension AppCoordinator {
+  @MainActor func attachCoordinators() {
+    attachHome()
+    attachSearchChallenge()
+    attachMyPage()
+    
     presenter.attachNavigationControllers(
       homeNavigationControllerable,
       searchChallengeNavigationControllerable,
       myPageNavigationControllerable
     )
+  }
+  
+  @MainActor func reloadAllTab() {
+    detachSearchChallenge()
+    detachMyPage()
     
-    addChild(homeCoordinator)
-    addChild(searchChallengeCoordinator)
-    addChild(myPageCoordinator)
+    homeCoorinator?.start()
+    attachSearchChallenge()
+    attachMyPage()
+  }
+}
+
+// MARK: - Home
+private extension AppCoordinator {
+  @MainActor func attachHome() {
+    guard homeCoorinator == nil else { return }
+
+    let coordinator = homeContainer.coordinator(
+      navigationControllerable: homeNavigationControllerable,
+      listener: self
+    )
+    addChild(coordinator)
+    self.homeCoorinator = coordinator
+  }
+}
+
+// MARK: - SearchChallenge
+private extension AppCoordinator {
+  @MainActor func attachSearchChallenge() {
+    guard searchChallengeCoordinator == nil else { return }
+    
+    let coordinator = searchChallengeContainer.coordinator(listener: self)
+    searchChallengeNavigationControllerable.setViewControllers([coordinator.viewControllerable])
+    addChild(coordinator)
+    self.searchChallengeCoordinator = coordinator
+  }
+  
+  @MainActor func detachSearchChallenge() {
+    guard let coordinator = searchChallengeCoordinator else { return }
+    removeChild(coordinator)
+    self.searchChallengeCoordinator = nil
+  }
+}
+
+// MARK: - MyPage
+private extension AppCoordinator {
+  @MainActor func attachMyPage() {
+    guard searchChallengeCoordinator == nil else { return }
+    
+    let coordinator = myPageContainer.coordinator(listener: self)
+    myPageNavigationControllerable.setViewControllers([coordinator.viewControllerable])
+    addChild(coordinator)
+    self.myPageCoordinator = coordinator
+  }
+  
+  @MainActor func detachMyPage() {
+    guard let coordinator = myPageCoordinator else { return }
+    removeChild(coordinator)
+    self.myPageCoordinator = nil
   }
 }
 
 // MARK: - HomeListener
-extension AppCoordinator: HomeListener { }
+extension AppCoordinator: HomeListener {
+  func requestLogInAtHome() {
+    Task { await attachLogIn(at: homeNavigationControllerable) }
+  }
+
+  func authenticatedFailedAtHome() {
+    Task { await reloadAllTab() }
+  }
+}
 
 // MARK: - SearchChallengeListener
 extension AppCoordinator: SearchChallengeListener { }
@@ -82,5 +176,20 @@ extension AppCoordinator: SearchChallengeListener { }
 extension AppCoordinator: MyPageListener {
   func isUserResigned() {
     presenter.changeNavigationControllerToHome()
+  }
+}
+
+// MARK: - LoginListener
+extension AppCoordinator: LogInListener {
+  func didFinishLogIn(userName: String) {
+    // TODO: - 환영합니다" 메시지 띄우기
+    Task {
+      await detachLogIn(willPopViewConroller: false)
+      await reloadAllTab()
+    }
+  }
+  
+  func didTapBackButtonAtLogIn() {
+    Task { await detachLogIn(willPopViewConroller: true) }
   }
 }
