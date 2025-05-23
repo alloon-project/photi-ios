d61e251da7111db0bfeb38f26dcd60d350668956
Jung SeokYoung <thrdud0423@gmail.com>
2025-05-23 22:11:54 +0900

    [FEAT]: 검색결과를 챌린지 이름으로 조회하는 ChallengeTitleResult Coordinator 추가

---
+++ Projects/Presentation/SearchChallenge/Implementations/SearchResult/ChallengeTitleResult/ChallengeTitleResultCoordinator.swift
@@ -0,0 +1,31 @@
+//
+//  ChallengeTitleResultCoordinator.swift
+//  HomeImpl
+//
+//  Created by jung on 5/23/25.
+//  Copyright © 2025 com.photi. All rights reserved.
+//
+
+import Core
+
+protocol ChallengeTitleResultListener: AnyObject { }
+
+protocol ChallengeTitleResultPresentable { }
+
+final class ChallengeTitleResultCoordinator: ViewableCoordinator<ChallengeTitleResultPresentable> {
+  weak var listener: ChallengeTitleResultListener?
+
+  private let viewModel: ChallengeTitleResultViewModel
+  
+  init(
+    viewControllerable: ViewControllerable,
+    viewModel: ChallengeTitleResultViewModel
+  ) {
+    self.viewModel = viewModel
+    super.init(viewControllerable)
+    viewModel.coordinator = self
+  }
+}
+
+// MARK: - ChallengeTitleResultCoordinatable
+extension ChallengeTitleResultCoordinator: ChallengeTitleResultCoordinatable { }
