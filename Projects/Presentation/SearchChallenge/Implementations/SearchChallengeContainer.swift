f0c540b9d1733c78756a47dbaf8038bd69457f75
정석영 <81402827+jungseokyoung-cloud@users.noreply.github.com>
2025-05-24 13:29:00 +0900

    [FEAT]: 검색화면 추천순 UI 구현 (#249)
    * [REFACT]: PhotiSearchBar 부모 클래스를 UISearchBar에 UITextField로 변경
    
    * [STYLE]: SearchNavigationBar 삭제로 인한 폴더구조 변경
    
    * [REFACT]: SegmentControl 확장성을 위해, 서브 클래스와 슈퍼 클래스로 분리
    
    * [FEAT]: CenterSegmentControl 추가
    
    * [REFACT]: SegmentControl 수정 사항 반영
    
    * [STYLE]: 불필요한 주석 제거
    
    * [BUG]: SetupUI 빠져있던 버그 수정
    
    * [FEAT]: 추천순 검색화면 Coordinator 추가
    
    * [FEAT]: 최신순 검색화면 Coordinator 추가
    
    * [FEAT]: 추천순, 인기순 각각 Segment 연결
    
    * [FEAT]: 챌린지 검색화면 기초 검색창 및 세그먼트 레이아웃 완료
    
    * [FEAT]: Core에 DashLineView 추가
    
    * [FEAT]: SearchChallenge에서 HeaderView 추가
    
    * [FEAT]: ChallengeCardCell 추가
    
    * [FEAT]: 검색화면 추천순에서 인기있는 챌린지 화면 구현
    
    * [REFACT]: TextChip 레이아웃 수정
    
    * [FEAT]: 추천순의 HashTagView 구현
    
    * [FEAT]: SearchChallenge에서 사용되는 HashTagCell 구현
    
    * [FEAT]: 추천순 UI 구현
    
    * [FEAT]: HashTagView Sticky하는 기능 구현
    
    * [FEAT]: HashTag 및 popularChallenge 뷰모델과 연결
    
    * [FEAT]: HashTah로 검색하는 TableView DIffableDataSource로 구현
    
    * [REFACT]: HashTag별 Challenge Pagination 구현
    
    * [FEAT]: 요청 중 HashTag 바뀌는 경우 에러처리 기능 추가

--- Projects/Presentation/SearchChallenge/Implementations/SearchChallengeContainer.swift
+++ Projects/Presentation/SearchChallenge/Implementations/SearchChallengeContainer.swift
@@ -16,15 +16,22 @@ public protocol SearchChallengeDependency: Dependency {
 
 public final class SearchChallengeContainer:
   Container<SearchChallengeDependency>,
-  SearchChallengeContainable {
+  SearchChallengeContainable,
+  RecommendedChallengesDependency,
+  RecentChallengesDependency {
   public func coordinator(listener: SearchChallengeListener) -> ViewableCoordinating {
     let viewModel = SearchChallengeViewModel()
     let viewControllerable = SearchChallengeViewController(viewModel: viewModel)
     
+    let recommendedChallenges = RecommendedChallengesContainer(dependency: self)
+    let recentChallenges = RecentChallengesContainer(dependency: self)
+    
     let coordinator = SearchChallengeCoordinator(
       viewControllerable: viewControllerable,
       viewModel: viewModel,
-      challengeOrganizeContainable: dependency.challengeOrganizeContainable
+      challengeOrganizeContainable: dependency.challengeOrganizeContainable,
+      recommendedChallengesContainable: recommendedChallenges,
+      recentChallengesContainable: recentChallenges
     )
     coordinator.listener = listener
     return coordinator
--- Projects/Presentation/SearchChallenge/Implementations/SearchChallengeCoordinator.swift
+++ Projects/Presentation/SearchChallenge/Implementations/SearchChallengeCoordinator.swift
@@ -10,7 +10,9 @@ import Core
 import Challenge
 import SearchChallenge
 
-protocol SearchChallengePresentable { }
+protocol SearchChallengePresentable {
+  func attachViewControllerables(_ viewControllerables: ViewControllerable...)
+}
 
 final class SearchChallengeCoordinator: ViewableCoordinator<SearchChallengePresentable> {
   weak var listener: SearchChallengeListener?
@@ -21,19 +23,48 @@ final class SearchChallengeCoordinator: ViewableCoordinator<SearchChallengePrese
   private let organizeContainable: ChallengeOrganizeContainable
   private var organizeCoordinator: Coordinating?
   
+  private let recommendedChallengesContainable: RecommendedChallengesContainable
+  private var recommendedChallengesCoordinator: ViewableCoordinating?
+  
+  private let recentChallengesContainable: RecentChallengesContainable
+  private var recentChallengesCoordinator: ViewableCoordinating?
+  
   init(
     viewControllerable: ViewControllerable,
     viewModel: SearchChallengeViewModel,
-    challengeOrganizeContainable: ChallengeOrganizeContainable
+    challengeOrganizeContainable: ChallengeOrganizeContainable,
+    recommendedChallengesContainable: RecommendedChallengesContainable,
+    recentChallengesContainable: RecentChallengesContainable
   ) {
     self.viewModel = viewModel
-    
     self.organizeContainable = challengeOrganizeContainable
-    
+    self.recommendedChallengesContainable = recommendedChallengesContainable
+    self.recentChallengesContainable = recentChallengesContainable
     super.init(viewControllerable)
     viewModel.coordinator = self
   }
   
+  override func start() {
+    attachSegments()
+  }
+  
+  func attachSegments() {
+    let recommendedChallenges = recommendedChallengesContainable.coordinator(listener: self)
+    let recentChallenges = recentChallengesContainable.coordinator(listener: self)
+    
+    presenter.attachViewControllerables(
+      recommendedChallenges.viewControllerable,
+      recentChallenges.viewControllerable
+    )
+    addChild(recommendedChallenges)
+    addChild(recentChallenges)
+    self.recommendedChallengesCoordinator = recommendedChallenges
+    self.recentChallengesCoordinator = recentChallenges
+  }
+}
+
+// MARK: - ChallengeOrganize
+private extension SearchChallengeCoordinator {
   func attachChallengeOrganize() {
     guard organizeCoordinator == nil else { return }
     
@@ -58,15 +89,22 @@ final class SearchChallengeCoordinator: ViewableCoordinator<SearchChallengePrese
   }
 }
 
+// MARK: - SearchChallengeCoordinatable
 extension SearchChallengeCoordinator: SearchChallengeCoordinatable {
   func didTapChallengeOrganize() {
     attachChallengeOrganize()
   }
 }
 
-// MARK: - Challenge Organize Listener
+// MARK: - ChallengeOrganizeListener
 extension SearchChallengeCoordinator: ChallengeOrganizeListener {
   func didTapBackButtonAtChallengeOrganize() {
     detachChallengeOrganize()
   }
 }
+
+// MARK: - RecommendedChallengesListener
+extension SearchChallengeCoordinator: RecommendedChallengesListener { }
+
+// MARK: - RecentChallengesListener
+extension SearchChallengeCoordinator: RecentChallengesListener { }
