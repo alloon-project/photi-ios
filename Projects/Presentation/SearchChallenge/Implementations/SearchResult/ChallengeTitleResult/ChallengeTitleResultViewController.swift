9c6642eb6d75c0c2ee620126c08f5a3fec5ecd6e
Jung SeokYoung <thrdud0423@gmail.com>
2025-05-26 20:07:38 +0900

    [FEAT]: 챌린지 이름순 검색 결과 UI 구현

--- Projects/Presentation/SearchChallenge/Implementations/SearchResult/ChallengeTitleResult/ChallengeTitleResultViewController.swift
+++ Projects/Presentation/SearchChallenge/Implementations/SearchResult/ChallengeTitleResult/ChallengeTitleResultViewController.swift
@@ -14,11 +14,38 @@ import Core
 import DesignSystem
 
 final class ChallengeTitleResultViewController: UIViewController, ViewControllerable {
+  enum Constants {
+    static let itemSpacing: CGFloat = 11
+    static let groupSpacing: CGFloat = 16
+    static let horizontalContentInset: CGFloat = 24
+  }
+  
+  typealias DataSourceType = UICollectionViewDiffableDataSource<Int, ResultChallengeCardPresentationModel>
+  typealias SnapShot = NSDiffableDataSourceSnapshot<Int, ResultChallengeCardPresentationModel>
+  
   // MARK: - Properties
   private let viewModel: ChallengeTitleResultViewModel
   private let disposeBag = DisposeBag()
+  private var datasource: DataSourceType?
   
   // MARK: - UI Components
+  private let emptyResultLabel: UILabel = {
+    let label = UILabel()
+    label.attributedText = "해당 챌린지가 없어요.".attributedString(font: .body2, color: .gray400)
+    
+    return label
+  }()
+  
+  private let challengeCollectionView: UICollectionView = {
+    let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewLayout())
+    collectionView.registerCell(ChallengeTitleResultCardCell.self)
+    collectionView.backgroundColor = .clear
+    collectionView.contentInsetAdjustmentBehavior = .never
+    collectionView.showsHorizontalScrollIndicator = false
+    collectionView.showsVerticalScrollIndicator = false
+
+    return collectionView
+  }()
   
   // MARK: - Initializers
   init(viewModel: ChallengeTitleResultViewModel) {
@@ -36,6 +63,11 @@ final class ChallengeTitleResultViewController: UIViewController, ViewController
     super.viewDidLoad()
     
     setupUI()
+    bind()
+    challengeCollectionView.collectionViewLayout = compositionalLayout()
+    let datasource = diffableDatasource()
+    self.datasource = datasource
+    challengeCollectionView.dataSource = datasource
   }
 }
 
@@ -46,9 +78,14 @@ private extension ChallengeTitleResultViewController {
     setConstraints()
   }
   
-  func setViewHierarchy() { }
+  func setViewHierarchy() {
+    view.addSubviews(emptyResultLabel, challengeCollectionView)
+  }
   
-  func setConstraints() { }
+  func setConstraints() {
+    challengeCollectionView.snp.makeConstraints { $0.edges.equalToSuperview() }
+    emptyResultLabel.snp.makeConstraints { $0.center.equalToSuperview() }
+  }
 }
 
 // MARK: - Bind Methods
@@ -68,3 +105,76 @@ private extension ChallengeTitleResultViewController {
 
 // MARK: - ChallengeTitleResultPresentable
 extension ChallengeTitleResultViewController: ChallengeTitleResultPresentable { }
+
+// MARK: - UICollectionViewLayout
+private extension ChallengeTitleResultViewController {
+  func compositionalLayout() -> UICollectionViewCompositionalLayout {
+    return .init { _, environment in
+      let itemSpacing = Constants.itemSpacing
+      let horizontalInset = Constants.horizontalContentInset
+      
+      let availableWidth = environment.container.effectiveContentSize.width
+      let itemWidth = ((availableWidth - horizontalInset * 2) - itemSpacing) / 2
+      let itemHeight = itemWidth * 1.15
+      
+      let itemSize = NSCollectionLayoutSize(
+        widthDimension: .absolute(itemWidth),
+        heightDimension: .absolute(itemHeight)
+      )
+      let item = NSCollectionLayoutItem(layoutSize: itemSize)
+      
+      let groupSize = NSCollectionLayoutSize(
+        widthDimension: .fractionalWidth(1),
+        heightDimension: .absolute(itemHeight)
+      )
+      let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item, item])
+      group.interItemSpacing = .fixed(itemSpacing)
+      let section = NSCollectionLayoutSection(group: group)
+      section.contentInsets = .init(
+        top: horizontalInset,
+        leading: horizontalInset,
+        bottom: 40,
+        trailing: horizontalInset
+      )
+      section.interGroupSpacing = Constants.groupSpacing
+      
+      return section
+    }
+  }
+}
+
+// MARK: - UICollectionViewDataSource
+private extension ChallengeTitleResultViewController {
+  func diffableDatasource() -> DataSourceType {
+    return .init(collectionView: challengeCollectionView) { collectionView, indexPath, model in
+      let cell = collectionView.dequeueCell(ChallengeTitleResultCardCell.self, for: indexPath)
+      cell.configure(with: model)
+      return cell
+    }
+  }
+  
+  func initialize(with models: [ResultChallengeCardPresentationModel]) {
+    guard let datasource else { return }
+    var snapshot = datasource.snapshot()
+    snapshot.deleteAllItems()
+    
+    snapshot = append(models: models, to: snapshot)
+    datasource.apply(snapshot)
+  }
+  
+  func append(models: [ResultChallengeCardPresentationModel]) {
+    guard let datasource else { return }
+    let snapshot = append(models: models, to: datasource.snapshot())
+    datasource.apply(snapshot)
+  }
+  
+  func append(models: [ResultChallengeCardPresentationModel], to snapshot: SnapShot) -> SnapShot {
+    var snapshot = snapshot
+    if !snapshot.sectionIdentifiers.contains(0) {
+      snapshot.appendSections([0])
+    }
+    models.forEach { snapshot.appendItems([$0]) }
+    
+    return snapshot
+  }
+}
