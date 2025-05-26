5e973a6f76f34ea33af36f50a3103ec79cddfb1b
Henry <wcbe9745@naver.com>
2025-05-26 22:56:51 +0900

    [FEAT]: ModifyCoordinator, Container 구현

---
+++ Projects/Presentation/Challenge/Implementations/Modify/ChallengeModifyCoordinator.swift
@@ -0,0 +1,125 @@
+//
+//  ChallengeModifyCoordinator.swift
+//  Presentation
+//
+//  Created by 임우섭 on 5/17/25.
+//  Copyright © 2025 com.photi. All rights reserved.
+//
+
+import Challenge
+import Core
+
+public protocol ModifyChallengeListener: AnyObject {}
+
+protocol ChallengeModifyPresentable {
+  func setLeftView(
+    title: String,
+    hashtags: [String],
+    verificationTime: String,
+    goal: String
+  )
+  
+  func setRightView(
+    image: UIImageWrapper,
+    rules: [String],
+    deadLine: String
+  )
+}
+
+final class ChallengeModifyCoordinator: ViewableCoordinator<ChallengeModifyPresentable> {
+  weak var listener: ModifyChallengeListener?
+
+  private let viewModel: ChallengeModifyViewModel
+  
+  private let modifyNameContainer: ModifyChallengeNameContainable
+  private var modifyNameCoordinator: ViewableCoordinating?
+  
+  private let modifyGoalContainer: ChallengeGoalContainable
+  private var modifyGoalCoordinator: ViewableCoordinating?
+  
+  private let modifyCoverContainer: ChallengeCoverContainable
+  private var modifyCoverCoordinator: ViewableCoordinating?
+  
+  private let modifyHashtagContainer: ChallengeHashtagContainable
+  private var modifyHashtagCoordinator: ViewableCoordinating?
+  
+  private let modifyRuleContainer: ChallengeRuleContainable
+  private var modifyRuleCoordinator: ViewableCoordinating?
+  
+  init(
+    viewControllerable: ViewControllerable,
+    viewModel: ChallengeModifyViewModel,
+    modifyNameContainer: ModifyChallengeNameContainable,
+    modifyGoalContainer: ChallengeGoalContainable,
+    modifyCoverContainer: ChallengeCoverContainable,
+    modifyHashtagContainer: ChallengeHashtagContainable,
+    modifyRuleContainer: ChallengeRuleContainable
+  ) {
+    self.modifyNameContainer = modifyNameContainer
+    self.modifyGoalContainer = modifyGoalContainer
+    self.modifyCoverContainer = modifyCoverContainer
+    self.modifyHashtagContainer = modifyHashtagContainer
+    self.modifyRuleContainer = modifyRuleContainer
+    self.viewModel = viewModel
+    super.init(viewControllerable)
+    viewModel.coordinator = self
+  }
+}
+
+// MARK: - Detach
+private extension ChallengeModifyCoordinator {
+  func detachModifyName() {
+    guard let coordinator = modifyNameCoordinator else { return }
+    
+    removeChild(coordinator)
+    self.modifyNameCoordinator = nil
+    viewControllerable.popViewController(animated: true)
+  }
+  
+  func detachModifyGoal() {
+    guard let coordinator = modifyGoalCoordinator else { return }
+    
+    removeChild(coordinator)
+    self.modifyGoalCoordinator = nil
+    viewControllerable.popViewController(animated: true)
+  }
+  
+  func detachModifyCover() {
+    guard let coordinator = modifyCoverCoordinator else { return }
+    
+    removeChild(coordinator)
+    self.modifyCoverCoordinator = nil
+    viewControllerable.popViewController(animated: true)
+  }
+  
+  func detachModifyHashtag() {
+    guard let coordinator = modifyHashtagCoordinator else { return }
+    
+    removeChild(coordinator)
+    self.modifyHashtagCoordinator = nil
+    viewControllerable.popViewController(animated: true)
+  }
+  
+  func detachModifyRule() {
+    guard let coordinator = modifyRuleCoordinator else { return }
+    
+    removeChild(coordinator)
+    self.modifyRuleCoordinator = nil
+    viewControllerable.popViewController(animated: true)
+  }
+}
+
+// MARK: - ChallengeModifyCoordinatable
+extension ChallengeModifyCoordinator: ChallengeModifyCoordinatable {
+  func attachModifyName() {}
+  
+  func attachModifyGoal() {}
+  
+  func attachModifyCover() {}
+  
+  func attachModifyHashtag() {}
+  
+  func attachModifyRule() {}
+  
+  func didTapBackButton() {}
+}
