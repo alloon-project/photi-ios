b2ebebb5b218cb62b8b258d5541441287a319505
Jung SeokYoung <thrdud0423@gmail.com>
2025-05-27 16:49:42 +0900

    [FEAT]: 추천순에 인기있는 챌린지, 해시태그 연결

---
+++ Projects/Presentation/SearchChallenge/Implementations/PresentationModel/SearchChallengePresentaionModelMapper.swift
@@ -0,0 +1,22 @@
+//
+//  SearchChallengePresentaionModelMapper.swift
+//  SearchChallengeImpl
+//
+//  Created by jung on 5/27/25.
+//  Copyright © 2025 com.photi. All rights reserved.
+//
+
+import Core
+import Entity
+
+struct SearchChallengePresentaionModelMapper {
+  func mapToChallengeCardPresentationModel(from challenge: ChallengeDetail) -> ChallengeCardPresentationModel {
+    return .init(
+      id: challenge.id,
+      hashTags: challenge.hashTags,
+      title: challenge.name,
+      imageUrl: challenge.imageUrl,
+      deadLine: challenge.endDate.toString("~ yyyy.MM.dd")
+    )
+  }
+}
