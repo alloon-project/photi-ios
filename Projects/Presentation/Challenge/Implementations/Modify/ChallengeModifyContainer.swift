b22f0759f9fe6a67612433edeb71b898d395b18f
Henry <wcbe9745@naver.com>
2025-06-01 22:43:44 +0900

    [FEAT]: 챌린지 수정 UI (#274)
    * [FEAT]: Challenge Modify VC UI 구현
    
    bind작업 전 입니다.
    
    * [FEAT]: ChallengeModify VM 구현
    
    * [FEAT]: ModifyCoordinator, Container 구현
    
    * [FEAT]: Challenge Modify UI 및 기초 구현

---
+++ Projects/Presentation/Challenge/Implementations/Modify/ChallengeModifyContainer.swift
@@ -0,0 +1,50 @@
+//
+//  ChallengeModifyContainer.swift
+//  Presentation
+//
+//  Created by 임우섭 on 5/17/25.
+//  Copyright © 2025 com.photi. All rights reserved.
+//
+
+import Challenge
+import Core
+import UseCase
+
+public protocol ChallengeModifyDependency: Dependency {
+  var organizeUseCase: OrganizeUseCase { get }
+}
+
+public final class ChallengeModifyContainer:
+  Container<ChallengeModifyDependency>,
+  ChallengeNameDependency,
+  ChallengeGoalDependency,
+  ChallengeCoverDependency,
+  ChallengeRuleDependency,
+  ChallengeHashtagDependency {
+  var organizeUseCase: OrganizeUseCase { dependency.organizeUseCase }
+
+  public func coordinator(
+    listener: ModifyChallengeListener
+  ) -> ViewableCoordinating {
+    let viewModel = ChallengeModifyViewModel(useCase: dependency.organizeUseCase)
+    let viewControllerable = ChallengeModifyViewController(viewModel: viewModel)
+    
+    let modifyChallengeNameContainable = ChallengeNameContainer(dependency: self)
+    let modifyChallengeGoalContainable = ChallengeGoalContainer(dependency: self)
+    let modifyChallengeCoverContainable = ChallengeCoverContainer(dependency: self)
+    let modifyChallengeRuleContainable = ChallengeRuleContainer(dependency: self)
+    let modifyChallengeHashtagContainable = ChallengeHashtagContainer(dependency: self)
+    
+    let coordinator = ChallengeModifyCoordinator(
+      viewControllerable: viewControllerable,
+      viewModel: viewModel,
+      modifyNameContainer: modifyChallengeNameContainable,
+      modifyGoalContainer: modifyChallengeGoalContainable,
+      modifyCoverContainer: modifyChallengeCoverContainable,
+      modifyHashtagContainer: modifyChallengeHashtagContainable,
+      modifyRuleContainer: modifyChallengeRuleContainable
+    )
+    coordinator.listener = listener
+    return coordinator
+  }
+}
