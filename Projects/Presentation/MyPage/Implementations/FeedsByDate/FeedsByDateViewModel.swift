b9e0317091768697ff4d6be257ddf7448c23eb14
Jung SeokYoung <thrdud0423@gmail.com>
2025-06-03 21:55:27 +0900

    [FEAT]: FeedsByDate에 fetch해오는 API 연동 완료

--- Projects/Presentation/MyPage/Implementations/FeedsByDate/FeedsByDateViewModel.swift
+++ Projects/Presentation/MyPage/Implementations/FeedsByDate/FeedsByDateViewModel.swift
@@ -6,7 +6,11 @@
 //  Copyright © 2025 com.photi. All rights reserved.
 //
 
+import Foundation
+import RxCocoa
 import RxSwift
+import Core
+import Entity
 import UseCase
 
 protocol FeedsByDateCoordinatable: AnyObject {
@@ -25,12 +29,19 @@ final class FeedsByDateViewModel: FeedsByDateViewModelType {
   private let disposeBag = DisposeBag()
   private let useCase: MyPageUseCase
   let date: Date
-
+  
+  private let feedsRelay = BehaviorRelay<[FeedsByDatePresentationModel]>(value: [])
+  
   // MARK: - Input
-  struct Input { }
+  struct Input {
+    let didTapBackButton: Signal<Void>
+    let requestData: Signal<Void>
+  }
   
   // MARK: - Output
-  struct Output { }
+  struct Output {
+    let feeds: Driver<[FeedsByDatePresentationModel]>
+  }
   
   // MARK: - Initializers
   init(date: Date, useCase: MyPageUseCase) {
@@ -44,7 +55,45 @@ final class FeedsByDateViewModel: FeedsByDateViewModelType {
         owner.coordinator?.didTapBackButton()
       }
       .disposed(by: disposeBag)
+    
+    input.requestData
+      .emit(with: self) { owner, _ in
+        Task { await owner.loadFeedsByDate() }
+      }
+      .disposed(by: disposeBag)
+    
+    return Output(feeds: feedsRelay.asDriver())
   }
 }
+
+// MARK: - API Methods
+private extension FeedsByDateViewModel {
+  func loadFeedsByDate() async {
+    let date = date.convertTimezone(from: .current, to: .kst).toString("YYYY-MM-dd")
+    
+    do {
+      let feeds = try await useCase.loadFeeds(byDate: date)
+      let models = feeds.map { mapToFeedsByDatePresentationModel($0) }
+      
+      feedsRelay.accept(models)
+    } catch {
+      // TODO: 에러 구현
+      print(error)
+    }
+  }
+}
+
+// MARK: - Private Methods
+private extension FeedsByDateViewModel {
+  func mapToFeedsByDatePresentationModel(_ feed: FeedSummary) -> FeedsByDatePresentationModel {
+    let provedTime = feed.proveTime.convertTimezone(from: .current, to: .kst)
+      .toString("HH:mm")
+    return .init(
+      challengeId: feed.challengeId,
+      feedId: feed.feedId,
+      feedImageViewUrl: feed.imageUrl,
+      title: feed.name,
+      provedTime: provedTime
+    )
   }
 }
