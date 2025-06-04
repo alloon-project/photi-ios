f090c5bb626b88fb6fd03cdcf4ac3931a257ee82
Jung SeokYoung <thrdud0423@gmail.com>
2025-06-04 13:57:31 +0900

    [REFACT]: 비밀번호 찾기 UI 수정

--- Projects/Presentation/LogIn/Implementations/FindPassword/FindPasswordViewModel.swift
+++ Projects/Presentation/LogIn/Implementations/FindPassword/FindPasswordViewModel.swift
@@ -29,27 +29,26 @@ final class FindPasswordViewModel: FindPasswordViewModelType {
   
   private let disposeBag = DisposeBag()
   private let useCase: LogInUseCase
-  private let invalidIdOrEmailRelay = PublishRelay<Void>()
+  private let unMatchedIdOrEmailIdOrEmailRelay = PublishRelay<Void>()
+  private let networkUnstableRelay = PublishRelay<Void>()
   
   // MARK: - Input
   struct Input {
-    let didTapBackButton: ControlEvent<Void>
-    let userId: ControlProperty<String>
-    let endEditingUserId: ControlEvent<Void>
-    let editingUserId: ControlEvent<Void>
-    let userEmail: ControlProperty<String>
-    let endEditingUserEmail: ControlEvent<Void>
-    let editingUserEmail: ControlEvent<Void>
-    let didTapNextButton: ControlEvent<Void>
+    let didTapBackButton: Signal<Void>
+    let userId: Driver<String>
+    let endEditingUserId: Signal<Void>
+    let userEmail: Driver<String>
+    let endEditingUserEmail: Signal<Void>
+    let didTapNextButton: Signal<Void>
   }
   
   // MARK: - Output
   struct Output { 
-    var isVaildId: Signal<Bool>
-    var isValidEmailForm: Signal<Bool>
-    var isOverMaximumText: Signal<Bool>
-    var isEnabledNextButton: Signal<Bool>
-    var invalidIdOrEmail: Signal<Void>
+    let inValidIdFormat: Signal<Void>
+    let inValidEmailFormat: Signal<Void>
+    let isEnabledNextButton: Signal<Bool>
+    let unMatchedIdOrEmail: Signal<Void>
+    let networkUnstable: Signal<Void>
   }
   
   // MARK: - Initializers
@@ -59,47 +58,37 @@ final class FindPasswordViewModel: FindPasswordViewModelType {
   
   func transform(input: Input) -> Output {
     input.didTapBackButton
-      .bind(with: self) { onwer, _ in
-        onwer.coordinator?.didTapBackButton()
+      .emit(with: self) { owner, _ in
+        owner.coordinator?.didTapBackButton()
       }.disposed(by: disposeBag)
-    
-    let isValidId = input.endEditingUserId
+        
+    let isValidIdFormat = input.endEditingUserId
       .withLatestFrom(input.userId)
-      .withUnretained(self)
-      .map { $0.1.isValidateId }
+      .map { $0.isValidateId }
     
-    let isValidEmailForm = input.endEditingUserEmail
+    let isValidEmailFormat = input.endEditingUserEmail
       .withLatestFrom(input.userEmail)
-      .withUnretained(self)
-      .filter { $0.1.count <= 100 && !$0.1.isEmpty }
-      .map { $0.1.isValidateEmail() }
-      
-    let isOverMaximumText = input.editingUserEmail
-      .withLatestFrom(input.userEmail)
-      .map { $0.count > 100 }
+      .map { $0.isValidateEmail() && $0.count <= 100 && !$0.isEmpty }
     
-    let idWithEmailValid = Observable.combineLatest(input.userEmail, input.userId)
+    let isEnabledConfirm = Observable.combineLatest(isValidIdFormat.asObservable(), isValidEmailFormat.asObservable())
+      .map { $0 && $1 }
     
-    let isEnabledConfirm = idWithEmailValid
-      .map { $0.0.isValidateEmail() && $0.1.isValidateId }
+    let idAndEmail = Observable.combineLatest(input.userId.asObservable(), input.userEmail.asObservable())
+      .asDriver(onErrorJustReturn: ("", ""))
     
-    let didTapNextButtonWithInfo = input.didTapNextButton
-      .withLatestFrom(idWithEmailValid)
-      .share()
-    
-    didTapNextButtonWithInfo
-      .filter { !$0.0.isEmpty && !$0.1.isEmpty }
-      .subscribe(with: self) { owner, info in
-        owner.findPassword(userEmail: info.0, userName: info.1)
+    input.didTapNextButton
+      .withLatestFrom(idAndEmail)
+      .emit(with: self) { owner, info in
+        Task { await owner.requestTempoaryPassword(id: info.0, email: info.1) }
       }
       .disposed(by: disposeBag)
     
     return Output(
-      isVaildId: isValidId.asSignal(onErrorJustReturn: false), 
-      isValidEmailForm: isValidEmailForm.asSignal(onErrorJustReturn: false),
-      isOverMaximumText: isOverMaximumText.asSignal(onErrorJustReturn: true),
+      inValidIdFormat: isValidIdFormat.filter { !$0 }.map { _ in () }.asSignal(),
+      inValidEmailFormat: isValidEmailFormat.filter { !$0 }.map { _ in () }.asSignal(),
       isEnabledNextButton: isEnabledConfirm.asSignal(onErrorJustReturn: false),
-      invalidIdOrEmail: invalidIdOrEmailRelay.asSignal()
+      unMatchedIdOrEmail: unMatchedIdOrEmailIdOrEmailRelay.asSignal(),
+      networkUnstable: networkUnstableRelay.asSignal()
     )
   }
 }
