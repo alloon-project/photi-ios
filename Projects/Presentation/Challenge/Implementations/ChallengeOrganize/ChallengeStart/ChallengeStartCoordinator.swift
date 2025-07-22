bc3c0f91fe339999ff95008ca3b20bd0d3a2cb61
Henry <wcbe9745@naver.com>
2025-07-22 20:39:01 +0900

    [STYLE]: lint 준수

--- Projects/Presentation/Challenge/Implementations/ChallengeOrganize/ChallengeStart/ChallengeStartCoordinator.swift
+++ Projects/Presentation/Challenge/Implementations/ChallengeOrganize/ChallengeStart/ChallengeStartCoordinator.swift
@@ -33,7 +33,6 @@ final class ChallengeStartCoordinator: ViewableCoordinator<ChallengeStartPresent
 extension ChallengeStartCoordinator: ChallengeStartCoordinatable {
   func didFisishChallengeStart() {
     listener?.didFinishChallengeStart()
-
   }
   
   func didTapBackButton() {
