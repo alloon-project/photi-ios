feb0001eb7a9157331257d7b1a1710af84feab09
정석영 <81402827+jungseokyoung-cloud@users.noreply.github.com>
2025-08-01 16:03:53 +0900

    [BUG]:release 모드 일때, configuration 파일 누락되는 버그 해결 (#335)

--- Projects/App/Project.swift
+++ Projects/App/Project.swift
@@ -9,76 +9,78 @@ import ProjectDescription
 import ProjectDescriptionHelpers
 
 let project = Project.make(
-	name: "Photi",
-	targets: [
-		.make(
-			name: "Photi-DEV",
-			product: .app,
-			bundleId: "com.photi.develop",
-			infoPlist: .file(path: .relativeToRoot("Projects/App/Info.plist")),
-			sources: ["Sources/**"],
-			resources: ["Resources/**"],
-			dependencies: [
-				.Project.Presentation.Home,
-				.Project.Presentation.HomeImpl,
-				.Project.Presentation.SearchChallenge,
-				.Project.Presentation.SearchChallengeImpl,
-				.Project.Presentation.MyPage,
-				.Project.Presentation.MyPageImpl,
-				.Project.Presentation.LogIn,
-				.Project.Presentation.LogInImpl,
-				.Project.Presentation.Challenge,
-				.Project.Presentation.ChallengeImpl,
-				.Project.Presentation.Report,
-				.Project.Presentation.ReportImpl,
-				.Project.Domain.UseCaseImpl,
-				.Project.Data.RepositoryImpl
-			],
-			settings: .settings(
-				base: [
-					"ASSETCATALOG_COMPILER_APPICON_NAME": "DevAppIcon",
-					"OTHER_LDFLAGS": "-ObjC"
-				],
-				configurations: [
-					.debug(name: .debug, xcconfig: "./xcconfigs/Photi.debug.xcconfig")
-				]
-			)
-		),
-		.make(
-			name: "Photi-PROD",
-			product: .app,
-			bundleId: "com.photi.product",
-			infoPlist: .file(path: .relativeToRoot("Projects/App/Info.plist")),
-			sources: ["Sources/**"],
-			resources: ["Resources/**"],
-			dependencies: [
-				.Project.Presentation.Home,
-				.Project.Presentation.HomeImpl,
-				.Project.Presentation.SearchChallenge,
-				.Project.Presentation.SearchChallengeImpl,
-				.Project.Presentation.MyPage,
-				.Project.Presentation.MyPageImpl,
-				.Project.Presentation.LogIn,
-				.Project.Presentation.LogInImpl,
-				.Project.Presentation.Challenge,
-				.Project.Presentation.ChallengeImpl,
-				.Project.Presentation.Report,
-				.Project.Presentation.ReportImpl,
-				.Project.Domain.UseCaseImpl,
-				.Project.Data.RepositoryImpl
-			],
-			settings: .settings(
-				base: [
-					"ASSETCATALOG_COMPILER_APPICON_NAME": "ProdAppIcon",
-					"OTHER_LDFLAGS": "-ObjC"
-				],
-				configurations: [
-					.debug(name: .debug, xcconfig: "./xcconfigs/Photi.release.xcconfig")
-				]
-			)
-		)
-	],
-	additionalFiles: [
-		"./xcconfigs/Photi.shared.xcconfig"
-	]
+  name: "Photi",
+  targets: [
+    .make(
+      name: "Photi-DEV",
+      product: .app,
+      bundleId: "com.photi.develop",
+      infoPlist: .file(path: .relativeToRoot("Projects/App/Info.plist")),
+      sources: ["Sources/**"],
+      resources: ["Resources/**"],
+      dependencies: [
+        .Project.Presentation.Home,
+        .Project.Presentation.HomeImpl,
+        .Project.Presentation.SearchChallenge,
+        .Project.Presentation.SearchChallengeImpl,
+        .Project.Presentation.MyPage,
+        .Project.Presentation.MyPageImpl,
+        .Project.Presentation.LogIn,
+        .Project.Presentation.LogInImpl,
+        .Project.Presentation.Challenge,
+        .Project.Presentation.ChallengeImpl,
+        .Project.Presentation.Report,
+        .Project.Presentation.ReportImpl,
+        .Project.Domain.UseCaseImpl,
+        .Project.Data.RepositoryImpl
+      ],
+      settings: .settings(
+        base: [
+          "ASSETCATALOG_COMPILER_APPICON_NAME": "DevAppIcon",
+          "OTHER_LDFLAGS": "-ObjC"
+        ],
+        configurations: [
+          .debug(name: .debug, xcconfig: "./xcconfigs/Photi.debug.xcconfig"),
+          .release(name: .release, xcconfig: "./xcconfigs/Photi.debug.xcconfig")
+        ]
+      )
+    ),
+    .make(
+      name: "Photi-PROD",
+      product: .app,
+      bundleId: "com.photi.product",
+      infoPlist: .file(path: .relativeToRoot("Projects/App/Info.plist")),
+      sources: ["Sources/**"],
+      resources: ["Resources/**"],
+      dependencies: [
+        .Project.Presentation.Home,
+        .Project.Presentation.HomeImpl,
+        .Project.Presentation.SearchChallenge,
+        .Project.Presentation.SearchChallengeImpl,
+        .Project.Presentation.MyPage,
+        .Project.Presentation.MyPageImpl,
+        .Project.Presentation.LogIn,
+        .Project.Presentation.LogInImpl,
+        .Project.Presentation.Challenge,
+        .Project.Presentation.ChallengeImpl,
+        .Project.Presentation.Report,
+        .Project.Presentation.ReportImpl,
+        .Project.Domain.UseCaseImpl,
+        .Project.Data.RepositoryImpl
+      ],
+      settings: .settings(
+        base: [
+          "ASSETCATALOG_COMPILER_APPICON_NAME": "ProdAppIcon",
+          "OTHER_LDFLAGS": "-ObjC"
+        ],
+        configurations: [
+          .debug(name: .debug, xcconfig: "./xcconfigs/Photi.release.xcconfig"),
+          .release(name: .release, xcconfig: "./xcconfigs/Photi.release.xcconfig")
+        ]
+      )
+    )
+  ],
+  additionalFiles: [
+    "./xcconfigs/Photi.shared.xcconfig"
+  ]
 )
