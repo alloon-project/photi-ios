5a62354d64d7a72e00bae0487aef00470dffa113
Jung SeokYoung <thrdud0423@gmail.com>
2025-08-20 17:58:57 +0900

    [FEAT]: App Repository 추가 및 앱 버전 확인 API 구현

---
+++ Projects/Data/Repository/Implementations/AppRepositoryImpl/AppRepositoryImpl.swift
@@ -0,0 +1,32 @@
+//
+//  AppRepositoryImpl.swift
+//  DTO
+//
+//  Created by jung on 8/15/25.
+//  Copyright © 2025 com.photi. All rights reserved.
+//
+
+import DTO
+import Entity
+import PhotiNetwork
+import Repository
+
+public class AppRepositoryImpl: AppRepository {
+  public init() { }
+  
+  public func fetchForceUpdateRequired(version: String) async throws -> Bool {
+    let provider = Provider<AppAPI>(stubBehavior: .never)
+    
+    do {
+      let result = try await provider.request(.appVersion(version), type: AppVersionResponseDTO.self).value
+      
+      if let data = result.data {
+        return data.forceUpdate
+      } else {
+       return false
+      }
+    } catch {
+      throw APIError.serverError
+    }
+  }
+}
