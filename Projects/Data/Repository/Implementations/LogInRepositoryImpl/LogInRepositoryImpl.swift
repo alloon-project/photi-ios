ffff701aba0e832fd52dd0ce0fd2616c7c9d1870
Jung SeokYoung <thrdud0423@gmail.com>
2025-10-21 02:19:54 +0900

    [REFACT]: LogInRepository에서 Rx의존성 제거

--- Projects/Data/Repository/Implementations/LogInRepositoryImpl/LogInRepositoryImpl.swift
+++ Projects/Data/Repository/Implementations/LogInRepositoryImpl/LogInRepositoryImpl.swift
@@ -6,7 +6,6 @@
 //  Copyright © 2024 com.photi. All rights reserved.
 //
 
-import RxSwift
 import Core
 import DataMapper
 import DTO
@@ -21,112 +20,94 @@ public struct LogInRepositoryImpl: LogInRepository {
     self.dataMapper = dataMapper
   }
 }
-  
+
 // MARK: - Public Methods
 public extension LogInRepositoryImpl {
-  func logIn(userName: String, password: String) -> Single<String> {
+  func logIn(userName: String, password: String) async throws -> String {
     let requestDTO = dataMapper.mapToLogInRequestDTO(userName: userName, password: password)
     
-    return requestUnAuthorizableAPI(
+    return try await requestUnAuthorizableAPI(
       api: LogInAPI.login(dto: requestDTO),
       responseType: LogInResponseDTO.self
-    )
-    .map { $0.username }
+    ).username
   }
   
-  func requestUserInformation(email: String) -> Single<Void> {
-    return requestUnAuthorizableAPI(
+  func requestUserInformation(email: String) async throws {
+    try await requestUnAuthorizableAPI(
       api: LogInAPI.findId(email: email),
       responseType: SuccessResponseDTO.self
     )
-    .map { _ in () }
   }
   
-  func requestTemporaryPassword(email: String, userName: String) -> Single<Void> {
+  func requestTemporaryPassword(email: String, userName: String) async throws {
     let requestDTO = dataMapper.mapToFindPasswordRequestDTO(userEmail: email, userName: userName)
     
-    return requestUnAuthorizableAPI(
+    try await requestUnAuthorizableAPI(
       api: LogInAPI.findPassword(dto: requestDTO),
       responseType: SuccessResponseDTO.self
     )
-    .map { _ in () }
   }
   
-  func updatePassword(from password: String, newPassword: String) -> Single<Void> {
+  func updatePassword(from password: String, newPassword: String) async throws {
     let requestDTO = UpdatePasswordRequstDTO(password: password, newPassword: newPassword)
     
-    return requestAuthorizableAPI(
+    try await requestAuthorizableAPI(
       api: LogInAPI.updatePassword(dto: requestDTO),
       responseType: SuccessResponseDTO.self
     )
-    .map { _ in () }
   }
 }
 
 // MARK: - Private Methods
 private extension LogInRepositoryImpl {
-  func requestUnAuthorizableAPI<T: Decodable>(
-    api: LogInAPI,
-    responseType: T.Type,
-    behavior: StubBehavior = .never
-  ) -> Single<T> {
-    Single.create { single in
-      Task {
-        do {
-          let provider = Provider<LogInAPI>(stubBehavior: behavior)
-          let result = try await provider
-            .request(api, type: responseType.self)
-          
-          if (200..<300).contains(result.statusCode), let data = result.data {
-            single(.success(data))
-          } else if result.statusCode == 401 {
-            single(.failure(APIError.loginFailed(reason: .invalidEmailOrPassword)))
-          } else if result.statusCode == 404 {
-            single(.failure(APIError.userNotFound))
-          } else {
-            single(.failure(APIError.serverError))
-          }
-        } catch {
-          single(.failure(error))
-        }
-      }
-      return Disposables.create()
-    }
-  }
-  
   @discardableResult
   func requestAuthorizableAPI<T: Decodable>(
     api: LogInAPI,
     responseType: T.Type,
     behavior: StubBehavior = .never
-  ) -> Single<T> {
-    return Single.create { single in
-      Task {
-        do {
-          let provider = Provider<LogInAPI>(
-            stubBehavior: behavior,
-            session: .init(interceptor: AuthenticationInterceptor())
-          )
-          
-          let result = try await provider.request(api, type: responseType.self)
-          if (200..<300).contains(result.statusCode), let data = result.data {
-            single(.success(data))
-          } else if result.statusCode == 401 {
-            single(.failure(APIError.loginFailed(reason: .invalidEmailOrPassword)))
-          } else if result.statusCode == 403 {
-            single(.failure(APIError.authenticationFailed))
-          } else {
-            single(.failure(APIError.serverError))
-          }
-        } catch {
-          if case NetworkError.networkFailed(reason: .interceptorMapping) = error {
-            single(.failure(APIError.authenticationFailed))
-          } else {
-            single(.failure(error))
-          }
-        }
+  ) async throws -> T {
+    do {
+      let provider = Provider<LogInAPI>(
+        stubBehavior: behavior,
+        session: .init(interceptor: AuthenticationInterceptor())
+      )
+      
+      let result = try await provider.request(api, type: responseType.self)
+      if (200..<300).contains(result.statusCode), let data = result.data {
+        return data
+      } else if result.statusCode == 401 {
+        throw APIError.loginFailed(reason: .invalidEmailOrPassword)
+      } else if result.statusCode == 403 {
+        throw APIError.authenticationFailed
+      } else {
+        throw APIError.serverError
       }
-      return Disposables.create()
+    } catch {
+      if case NetworkError.networkFailed(reason: .interceptorMapping) = error {
+        throw APIError.authenticationFailed
+      } else {
+        throw error
+      }
+    }
+  }
+  
+  @discardableResult
+  func requestUnAuthorizableAPI<T: Decodable>(
+    api: LogInAPI,
+    responseType: T.Type,
+    behavior: StubBehavior = .never
+  ) async throws -> T {
+    let provider = Provider<LogInAPI>(stubBehavior: behavior)
+    let result = try await provider.request(api, type: responseType.self)
+    
+    if (200..<300).contains(result.statusCode), let data = result.data {
+      return data
+    } else if result.statusCode == 401 {
+      throw APIError.loginFailed(reason: .invalidEmailOrPassword)
+    } else if result.statusCode == 404 {
+      throw APIError.userNotFound
+    } else {
+      throw APIError.serverError
     }
   }
 }
