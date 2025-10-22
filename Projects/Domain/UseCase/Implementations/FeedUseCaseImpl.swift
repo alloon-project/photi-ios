d25554dba44b242c465558e07d287c818a3c6907
lemona-97 <wcbe9745@naver.com>
2025-10-22 20:33:46 +0900

    [REFACT]: Feed UseCase RxSwift 의존성 제거

--- Projects/Domain/UseCase/Implementations/FeedUseCaseImpl.swift
+++ Projects/Domain/UseCase/Implementations/FeedUseCaseImpl.swift
@@ -6,7 +6,6 @@
 //  Copyright © 2025 com.photi. All rights reserved.
 //
 
-import RxSwift
 import Entity
 import Repository
 import UseCase
@@ -52,25 +51,8 @@ public struct FeedUseCaseImpl: FeedUseCase {
     try await repository.deleteFeedComment(challengeId: challengeId, feedId: feedId, commentId: commentId)
   }
   
-  public func deleteFeed(challengeId: Int, feedId: Int) -> Single<Void> {
-    asyncToSingle { 
-      return try await repository.deleteFeed(challengeId: challengeId, feedId: feedId)
-    }
+  public func deleteFeed(challengeId: Int, feedId: Int) async throws -> Void {
+    return try await repository.deleteFeed(challengeId: challengeId, feedId: feedId)
   }
 }
 
-// MARK: - Private Methods
-private extension FeedUseCaseImpl {
-  func asyncToSingle<T>(_ work: @escaping () async throws -> T) -> Single<T> {
-    Single.create { single in
-      let task = Task {
-        do {
-          single(.success(try await work()))
-        } catch {
-          single(.failure(error))
-        }
-      }
-      return Disposables.create { task.cancel() }
-    }
-  }
-}
