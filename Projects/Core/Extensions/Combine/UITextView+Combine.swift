f3691877e5d3be6236a9c5126db70de6527733a1
Jung SeokYoung <thrdud0423@gmail.com>
2025-11-02 23:49:56 +0900

    [REFACT]: LineTextView에 Rx의존성 제거

---
+++ Projects/Core/Extensions/Combine/UITextView+Combine.swift
@@ -0,0 +1,29 @@
+//
+//  TextView+Combine.swift
+//  Core
+//
+//  Created by jung on 11/2/25.
+//  Copyright © 2025 com.photi. All rights reserved.
+//
+
+import UIKit
+import Combine
+
+public extension UITextView {
+  /// 현재 값 1회 + 이후 `textDidChangeNotification` 마다 텍스트를 방출합니다.
+  /// - Note: UI 업데이트 안전을 위해 메인 런루프로 전달하고, 동일 값은 중복 방출하지 않습니다.
+  var textPublisher: AnyPublisher<String, Never> {
+    let initial = Just(self.text ?? "").eraseToAnyPublisher()
+
+    let changes = NotificationCenter.default
+      .publisher(for: UITextView.textDidChangeNotification, object: self)
+      .map { [weak self] _ in self?.text ?? "" }
+      .eraseToAnyPublisher()
+
+    return initial
+      .merge(with: changes)
+      .removeDuplicates()
+      .receive(on: RunLoop.main)
+      .eraseToAnyPublisher()
+  }
+}
