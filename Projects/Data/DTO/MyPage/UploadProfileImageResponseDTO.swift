43bf9a838e5a057ff752b9ca7e2ce1c1cf26a3bf
Jung SeokYoung <thrdud0423@gmail.com>
2026-02-16 22:29:41 +0900

    [FEAT]: 프로필 이미지 변경시에 Response DTO 추가

---
+++ Projects/Data/DTO/MyPage/UploadProfileImageResponseDTO.swift
@@ -0,0 +1,11 @@
+//
+//  UploadProfileImageResponseDTO.swift
+//  DTO
+//
+//  Created by jung on 2/16/26.
+//  Copyright © 2026 com.photi. All rights reserved.
+//
+
+public struct UploadProfileImageResponseDTO: Decodable {
+  public let imageUrl: String
+}
