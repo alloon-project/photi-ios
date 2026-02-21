82cf17fcd5fcfc457f9d21f8385e43a58d34db01
정석영 <81402827+jungseokyoung-cloud@users.noreply.github.com>
2026-02-21 21:40:39 +0900

    [FEAT]: SNS 간편 로그인 UI 구현 (#397)

---
+++ Projects/Presentation/LogIn/Implementations/Views/OAuthLogInView.swift
@@ -0,0 +1,72 @@
+//
+//  OAuthLogInView.swift
+//  LogInImpl
+//
+//  Created by jung on 2/16/26.
+//  Copyright © 2026 com.photi. All rights reserved.
+//
+
+import UIKit
+import CoreUI
+import DesignSystem
+
+final class OAuthLogInView: UIView {
+  private let label: UILabel = {
+    let label = UILabel()
+    label.attributedText = "SNS 간편 로그인".attributedString(
+      font: .body1Bold,
+      color: .gray900
+    )
+    return label
+  }()
+  
+  private let stackView: UIStackView = {
+    let stackView = UIStackView()
+    stackView.spacing = 20
+    stackView.axis = .horizontal
+    stackView.distribution = .fillEqually
+    stackView.alignment = .center
+    
+    return stackView
+  }()
+  
+  private let appleLoginButton = RoundImageButton(image: .appleBlack)
+  private let kakaoLoginButton = RoundImageButton(image: .kakaoTalk)
+  private let googleLoginButton = RoundImageButton(image: .google)
+
+  // MARK: - Initializers
+  init() {
+    super.init(frame: .zero)
+    setupUI()
+  }
+  
+  @available(*, unavailable)
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
+
+// MARK: - UI Methods
+private extension OAuthLogInView {
+  func setupUI() {
+    setViewHeirarchy()
+    setConstraints()
+  }
+  
+  func setViewHeirarchy() {
+    addSubviews(label, stackView)
+    stackView.addArrangedSubviews(appleLoginButton, kakaoLoginButton, googleLoginButton)
+  }
+  
+  func setConstraints() {
+    label.snp.makeConstraints {
+      $0.top.centerX.equalToSuperview()
+    }
+    
+    stackView.snp.makeConstraints {
+      $0.centerX.bottom.equalToSuperview()
+      $0.top.equalTo(label.snp.bottom).offset(12)
+      $0.height.equalTo(40)
+    }
+  }
+}
