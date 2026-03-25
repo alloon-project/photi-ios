62620e62df354ce20f66431b0e49dfc2933b97d6
wooseob <wcbe9745@naver.com>
2026-03-25 16:31:33 +0900

    [REFACT]:  claude 작업 및 Login View Model Foundation추가

--- Tuist/Dependencies.swift
+++ Tuist/Dependencies.swift
@@ -43,7 +43,7 @@ let dependencies = Dependencies(
         ),
         .remote(
           url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git",
-          requirement: .upToNextMajor(from: "11.0.0")
+          requirement: .upToNextMajor(from: "13.1.0")
         ),
         .remote(
           url: "https://github.com/googleads/swift-package-manager-google-user-messaging-platform.git",
